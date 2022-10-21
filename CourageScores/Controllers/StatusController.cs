831704015340401d84fabf94585e6ab39ad3832b
Simon Laing <laingster@gmail.com>
2022-10-21 07:38:37 +0100

    Add empty controller

---
+++ CourageScores/Controllers/StatusController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourageScores.Controllers;
+
+[ApiController]
+public class StatusController : Controller
+{
+    [HttpGet("/api/Status")]
+    public async Task<string> Status()
+    {
+        return "OK";
+    }
+}
