a7e7159a5aa1d6a539798f80d88de80e910aa711
Simon Laing <laingster@gmail.com>
2022-10-26 20:33:23 +0100

    Add division component

---
+++ CourageScores/ClientApp/src/components/Division.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Division extends Component {
+    render() {
+        return (
+            <div>
+                <h2>CourageScores - Division</h2>
+            </div>
+        );
+    }
+}
