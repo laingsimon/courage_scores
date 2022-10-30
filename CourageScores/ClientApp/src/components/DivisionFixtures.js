ae51bde4b7162ec63a3e8fb6c915c6433e913e3e
Simon Laing <laingsimon@users.noreply.github.com>
2022-10-30 10:38:23 +0000

    Update DivisionFixtures.js

--- CourageScores/ClientApp/src/components/DivisionFixtures.js
+++ CourageScores/ClientApp/src/components/DivisionFixtures.js
@@ -3,9 +3,25 @@ import {useParams} from "react-router-dom";
 
 export function DivisionFixtures(props) {
     const {divisionId} = useParams();
-    const division = props.divisions[divisionId];
+    const divisionData = props.divisionData[divisionId];
 
-    return (<div>
-        Fixtures...
-    </div>);
+    if (!divisionData) {
+        props.apis.reloadDivision(divisionId); // don't await the async?
+        return (<div>Loading division data</div>);
+    }
+
+    return (divisionData.fixtureDates.map(date => (<div>
+       <h4>{date.date}</h4>
+       <table>
+          <tbody>
+             {date.fixtures.map(f => (<tr>
+                   <td>{f.homeTeam}</td>
+                   <td>{f.homeScore}</td>
+                   <td>vs</td>
+                   <td>{f.awayTeam}</td>
+                   <td>{f.awayScore}</td>
+                </tr>))}
+          </tbody>
+       </table>
+    </div>)));
 }
--- CourageScores/ClientApp/src/components/DivisionPlayers.js
+++ CourageScores/ClientApp/src/components/DivisionPlayers.js
@@ -3,9 +3,41 @@ import {useParams} from "react-router-dom";
 
 export function DivisionPlayers(props) {
     const {divisionId} = useParams();
-    const division = props.divisions[divisionId];
+    const divisionData = props.divisionData[divisionId];
+
+    if (!divisionData) {
+        props.apis.reloadDivision(divisionId); // don't await the async?
+        return (<div>Loading division data</div>);
+    }
 
     return (<div>
-        Players...
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Player</th>
+                    <th>Venue</th>
+                    <th>Won</th>
+                    <th>Lost</th>
+                    <th>Points</th>
+                    <th>Win %</th>
+                    <th>180s</th>
+                    <th>hi-check</th>
+                </tr>
+            </thead>
+            <tbody>
+            {divisionData.teams.map(p => (<tr>
+                <td>{p.rank}</td>
+                <td>{p.name}</td>
+                <td>{p.team}</td>
+                <td>{p.won}</td>
+                <td>{p.lost}</td>
+                <td>{p.points}</td>
+                <td>{p.winPc}</td>
+                <td>{p.oneEighties}</td>
+                <td>{p.over100checkouts}</td>
+            </tr>))}
+            </tbody>
+        </table>
     </div>);
 }
