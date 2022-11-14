30db8d0d2702ef0185bd8daf38f31397e7095e6d
Simon Laing <laingster@gmail.com>
2022-11-14 19:17:19 +0000

    Update styles for division and season editing

--- CourageScores/ClientApp/src/components/Division.js
+++ CourageScores/ClientApp/src/components/Division.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {Link, useParams} from "react-router-dom";
-import {NavItem, NavLink} from "reactstrap";
+import {ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle, NavItem, NavLink} from "reactstrap";
 import {DivisionTeams} from "./DivisionTeams";
 import {DivisionFixtures} from "./DivisionFixtures";
 import {DivisionPlayers} from "./DivisionPlayers";
@@ -9,7 +9,7 @@ import {Http} from "../api/http";
 import {DivisionApi} from "../api/division";
 import {SeasonApi} from "../api/season";
 
-export function Division({ account }) {
+export function Division({ account, apis }) {
     const { divisionId, mode } = useParams();
     const [ divisionData, setDivisionData ] = useState(null);
     const [ loading, setLoading ] = useState(false);
@@ -20,6 +20,7 @@ export function Division({ account }) {
     const [ seasonData, setSeasonData ] = useState(null);
     const [ divisionName, setDivisionName ] = useState(null);
     const [ updatingData, setUpdatingData ] = useState(false);
+    const [ seasonsDropdownOpen, setSeasonsDropdownOpen ] = useState(false);
 
     async function reloadDivisionData() {
         const api = new DivisionApi(new Http(new Settings()));
@@ -72,12 +73,17 @@ export function Division({ account }) {
     }
 
     async function saveSeasonDetails() {
+        if (updatingData) {
+            return;
+        }
+
         try {
             setUpdatingData(true);
             const api = new SeasonApi(new Http(new Settings()));
             const result = await api.update(seasonData);
 
             if (result.success) {
+                await apis.reloadAll();
                 await reloadDivisionData();
                 setEditMode(null);
             } else {
@@ -90,6 +96,10 @@ export function Division({ account }) {
     }
 
     async function saveDivisionName() {
+        if (updatingData) {
+            return;
+        }
+
         try {
             setUpdatingData(true);
             const api = new DivisionApi(new Http(new Settings()));
@@ -99,6 +109,7 @@ export function Division({ account }) {
             });
 
             if (result.success) {
+                await apis.reloadAll();
                 await reloadDivisionData();
                 setEditMode(null);
             } else {
@@ -115,29 +126,58 @@ export function Division({ account }) {
     }
 
     return (<div>
-        <h2>
+        <div className="btn-group py-2">
+            {editMode !== 'division' ? (<button className={`btn ${isDivisionAdmin ? 'btn-info' : 'btn-light'} text-nowrap`} onClick={() => isDivisionAdmin ? setEditMode('division') : null}>
+                {divisionName}
+                {isDivisionAdmin ? '✏' : ''}
+            </button>) : null}
             {editMode === 'division'
-                ? (<span className="h4">
-                    <input disabled={updatingData} value={divisionName} onChange={updateDivisionName} className="margin-right" />
-                    <button className="btn btn-sm btn-primary margin-right" onClick={saveDivisionName}>Save</button>
-                    <button className="btn btn-sm btn-secondary" onClick={() => setEditMode(null)}>Cancel</button>
-                </span>)
-                : (<span>{divisionData.name} {isDivisionAdmin ? (<span className="btn btn-sm extra-small" onClick={() => setEditMode('division')}>✏️</span>) : null}
-                </span>)},
+                ? (<div className="input-group margin-right">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">Name</span>
+                    </div>
+                    <input readOnly={updatingData} value={divisionName} onChange={updateDivisionName} className="no-border margin-right" />
+                    <button className="btn btn-sm btn-primary" onClick={saveDivisionName}>
+                        {updatingData ? (<span className="spinner-border spinner-border-sm margin-right" role="status" aria-hidden="true"></span>) : null}
+                        Save
+                    </button>
+                    {updatingData ? null : (<button className="btn btn-sm btn-warning" onClick={() => setEditMode(null)}>Cancel</button>)}
+                </div>)
+                : null}
+            {editMode !== 'season' ? (<ButtonDropdown isOpen={seasonsDropdownOpen} toggle={() => setSeasonsDropdownOpen(!seasonsDropdownOpen)}>
+                    <button className={`btn ${isDivisionAdmin ? 'btn-info' : 'btn-light'} text-nowrap`} onClick={() => isSeasonAdmin ? setEditMode('season') : null}>
+                        {seasonData.name} ({renderDate(divisionData.season.startDate)} - {renderDate(divisionData.season.endDate)})
+                        {isSeasonAdmin ? '✏' : ''}
+                    </button>
+                    <DropdownToggle caret color="info">
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem>{seasonData.name} ({renderDate(divisionData.season.startDate)} - {renderDate(divisionData.season.endDate)})</DropdownItem>
+                    </DropdownMenu>
+                </ButtonDropdown>
+            ) : null}
             {editMode === 'season'
-                ? (<span className="h4">
-                    <input disabled={updatingData} onChange={updateSeasonData} name="name" value={seasonData.name}/>
-                    <input disabled={updatingData} onChange={updateSeasonData} name="startDate" value={seasonData.startDate} type="date"/>
-                    -
-                    <input disabled={updatingData} onChange={updateSeasonData} name="endDate" value={seasonData.endDate} type="date"/>
-                    <button className="btn btn-sm btn-primary margin-right" onClick={saveSeasonDetails}>Save</button>
-                    <button className="btn btn-sm btn-secondary" onClick={() => setEditMode(null)}>Cancel</button>
-                </span>)
-                : (<span>{divisionData.season.name} ({renderDate(divisionData.season.startDate)} - {renderDate(divisionData.season.endDate)}) {isSeasonAdmin
-                    ? (<span className="btn btn-sm extra-small" onClick={() => setEditMode('season')}>✏️</span>)
-                    : null}
-                </span>)}
-        </h2>
+                ? (<div className="input-group margin-left">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">Name</span>
+                    </div>
+                    <input readOnly={updatingData} onChange={updateSeasonData} name="name" value={seasonData.name} className="no-border margin-right"/>
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">From</span>
+                    </div>
+                    <input readOnly={updatingData} onChange={updateSeasonData} name="startDate" value={seasonData.startDate} type="date" className="no-border margin-right"/>
+                    <div className="input-group-prepend">
+                        <span className="input-group-text">To</span>
+                    </div>
+                    <input readOnly={updatingData} onChange={updateSeasonData} name="endDate" value={seasonData.endDate} type="date" className="no-border margin-right"/>
+                    <button className="btn btn-sm btn-primary" onClick={saveSeasonDetails}>
+                        {updatingData ? (<span className="spinner-border spinner-border-sm margin-right" role="status" aria-hidden="true"></span>) : null}
+                        Save
+                    </button>
+                    {updatingData ? null : (<button className="btn btn-sm btn-warning" onClick={() => setEditMode(null)}>Cancel</button>)}
+                </div>)
+                : null}
+        </div>
         <ul className="nav nav-tabs">
             <NavItem>
                 <NavLink tag={Link} className={effectiveTab === 'teams' ? ' text-dark active' : 'text-light'} to={`/division/${divisionId}/teams`}>Teams</NavLink>
