e216899165f8e94ef2f796ffe176447b610f28e1
Simon Laing <laingster@gmail.com>
2022-11-16 21:06:42 +0000

    Fixes to bootstrap dropdown display

--- CourageScores/ClientApp/src/components/BootstrapDropdown.js
+++ CourageScores/ClientApp/src/components/BootstrapDropdown.js
@@ -5,18 +5,20 @@ export function BootstrapDropdown({value, onChange, options, color, className, d
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
     if (options.length === 0) {
-        return (<button className={`btn btn-sm btn-${color || 'light'}`} disabled>{value}</button>)
-    }
-
-    if (options.length === 1 || disabled) {
-        return (<button className={`btn btn-sm btn-${color || 'light'}`} disabled>{options[0].text}</button>)
+        return (<button className={`btn btn-sm btn-${color || 'light'} dropdown-toggle`} disabled>&nbsp;</button>)
     }
 
     const selectedOption = options.filter(o => o.value === value)[0];
 
+    if (disabled) {
+        return selectedOption
+            ? (<button className={`btn btn-sm btn-${color} dropdown-toggle`} disabled>{selectedOption.text}</button>)
+            : (<button className={`btn btn-sm btn-${color} dropdown-toggle`} disabled></button>)
+    }
+
     return (<ButtonDropdown isOpen={dropdownOpen} toggle={() => setDropdownOpen(!dropdownOpen)} className={className}>
-        <DropdownToggle caret color={color || 'light'} className="btn-sm">
-            {selectedOption ? selectedOption.text : value}
+        <DropdownToggle caret color={color || 'light'} className="btn-sm" style={{ minWidth: '100px' }}>
+            {selectedOption ? selectedOption.text || value : value}
         </DropdownToggle>
         <DropdownMenu>
             {options.map(o => (<DropdownItem key={o.value}
