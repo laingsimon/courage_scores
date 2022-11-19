e995b08ace85001f164a8955b7354d4a4519b390
Simon Laing <laingsimon@users.noreply.github.com>
2022-11-19 11:45:56 +0000

    Update AddOrUpdateTeamCommand.cs

--- CourageScores/Services/Command/AddOrUpdateTeamCommand.cs
+++ CourageScores/Services/Command/AddOrUpdateTeamCommand.cs
@@ -4,11 +4,14 @@ namespace CourageScores.Services.Command;
 
 public class AddOrUpdateTeamCommand : AddOrUpdateCommand<Models.Cosmos.Team.Team, EditTeamDto>
 {
-    protected override Task ApplyUpdates(Models.Cosmos.Team.Team team, EditTeamDto update, CancellationToken token)
+    protected override async Task ApplyUpdates(Models.Cosmos.Team.Team team, EditTeamDto update, CancellationToken token)
     {
+        // TODO: find the fixtures in the given season & division (update.SeasonId & update.DivisionId) for the given team (team.Id)
+        // TODO: update each fixture home/away name and address
+        // TODO: abort if the address becomes the same for both home and away
+
         team.Name = update.Name;
         team.Address = update.Address;
         team.DivisionId = update.DivisionId;
-        return Task.CompletedTask;
     }
 }
