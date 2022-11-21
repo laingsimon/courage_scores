fdc15c9743fd10829bc7eb237ac1799cce6c4a14
Simon Laing <laingster@gmail.com>
2022-11-21 07:46:29 +0000

    Render server validation errors in ErrorDisplay

--- CourageScores/ClientApp/src/components/ErrorDisplay.js
+++ CourageScores/ClientApp/src/components/ErrorDisplay.js
@@ -3,6 +3,14 @@ import React from 'react';
 export function ErrorDisplay({ errors, messages, warnings, result, success, onClose, title }) {
     let index = 0;
 
+    function renderValidationErrors(errors) {
+        return (<ol className="text-danger">
+            {Object.keys(errors).map(key => {
+                return (<li key={key}>{key} {errors[key].map(e => (<p>{e}</p>))}</li>)
+            })}
+        </ol>)
+    }
+
     return (<div>
         <div className="modal fade show" role="dialog" style={{display: 'block'}}>
             <div className="modal-dialog modal-dialog-centered">
@@ -11,7 +19,8 @@ export function ErrorDisplay({ errors, messages, warnings, result, success, onCl
                         <h5>{title || 'There was an error'}</h5>
                     </div>
                     <div className="modal-body">
-                        {errors ? errors.map(e => (<p key={index++} className="text-danger">{e}</p>)) : null}
+                        {errors && errors.length ? errors.map(e => (<p key={index++} className="text-danger">{e}</p>)) : null}
+                        {errors && !errors.length ? (renderValidationErrors(errors)): null}
                         {warnings ? warnings.map(w => (<p key={index++} className="text-warning">{w}</p>)) : null}
                         {messages ? messages.map(m => (<p key={index++} className="text-primary">{m}</p>)) : null}
                     </div>
