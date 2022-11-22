bc95d9ce3729b7a51adc1644385ae0b827fa8045
Simon Laing <laingsimon@users.noreply.github.com>
2022-11-22 15:07:33 +0000

    Use dialog to display error details

--- CourageScores/ClientApp/src/components/ErrorDisplay.js
+++ CourageScores/ClientApp/src/components/ErrorDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Dialog} from "./Dialog";
 
 export function ErrorDisplay({ errors, messages, warnings, result, success, onClose, title }) {
     let index = 0;
@@ -11,25 +12,12 @@ export function ErrorDisplay({ errors, messages, warnings, result, success, onCl
         </ol>)
     }
 
-    return (<div>
-        <div className="modal fade show" role="dialog" style={{display: 'block'}}>
-            <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content">
-                    <div className="modal-header justify-content-center">
-                        <h5>{title || 'There was an error'}</h5>
-                    </div>
-                    <div className="modal-body">
-                        {errors && errors.length ? errors.map(e => (<p key={index++} className="text-danger">{e}</p>)) : null}
-                        {errors && !errors.length ? (renderValidationErrors(errors)): null}
-                        {warnings ? warnings.map(w => (<p key={index++} className="text-warning">{w}</p>)) : null}
-                        {messages ? messages.map(m => (<p key={index++} className="text-primary">{m}</p>)) : null}
-                    </div>
-                    <div className="modal-footer">
-                        <button className="btn btn-primary" onClick={() => onClose ? onClose() : null}>Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="modal-backdrop fade show"></div>
-    </div>)
+    return (<Dialog onClose={onClose} title={title || 'There was an error'}>
+        <div>
+             {errors && errors.length ? errors.map(e => (<p key={index++} className="text-danger">{e}</p>)) : null}
+             {errors && !errors.length ? (renderValidationErrors(errors)): null}
+             {warnings ? warnings.map(w => (<p key={index++} className="text-warning">{w}</p>)) : null}
+             {messages ? messages.map(m => (<p key={index++} className="text-primary">{m}</p>)) : null}
+       </div>
+       </Dialog>);
 }
