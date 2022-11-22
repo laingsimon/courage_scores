e9f56938ce1021cd41299eca6fed0c575a7cc3a3
Simon Laing <laingsimon@users.noreply.github.com>
2022-11-22 14:59:20 +0000

    Conditionally show notes

--- CourageScores/ClientApp/src/components/MultiPlayerSelectionWithNotes.js
+++ CourageScores/ClientApp/src/components/MultiPlayerSelectionWithNotes.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {PlayerSelection} from "./PlayerSelection";
 
-export function MultiPlayerSelectionWithNotes({ onAddPlayer, players, disabled, allPlayers, onRemovePlayer, readOnly }) {
+export function MultiPlayerSelectionWithNotes({ onAddPlayer, players, disabled, allPlayers, onRemovePlayer, readOnly, showNotes }) {
     let index = 0;
     const [player, setPlayer] = useState(null);
     const [notes, setNotes] = useState('');
@@ -19,17 +19,25 @@ export function MultiPlayerSelectionWithNotes({ onAddPlayer, players, disabled, 
         alert('Ensure a player is selected first');
     }
 
+    function renderPlayer(p) {
+       if (showNotes) {
+           return `${p.name} (${p.notes})`;
+       }
+
+       return p.name;
+    }
+
     return (<div>
         <ol>
-            {(players || []).map(p => { index++; return (<li key={index}>{disabled ? <span>{p.name} ({p.notes})</span> : (<button
+            {(players || []).map(p => { index++; return (<li key={index}>{disabled ? <span>{renderPlayer(p)}</span> : (<button
                 disabled={disabled || readOnly}
                 className={`btn btn-sm ${disabled ? 'btn-secondary' : 'btn-primary'} margin-right`}
                 onClick={() => onRemovePlayer(p.id, index - 1)}>
-            {p.name} ({p.notes}) {disabled ? '' : '×'}
+            {renderPlayer(p)} {disabled ? '' : '×'}
             </button>)}</li>); })}
         </ol>
         {disabled ? null : (<div>
-            <input
+            {showNotes ? (<input
                 disabled={disabled}
                 readOnly={readOnly}
                 onChange={(elem) => setNotes(elem.target.value)}
@@ -37,7 +45,7 @@ export function MultiPlayerSelectionWithNotes({ onAddPlayer, players, disabled, 
                 className="margin-right"
                 type="number"
                 min="100"
-                max="120" />
+                max="120" />) : null}
             <PlayerSelection
                 disabled={disabled}
                 readOnly={readOnly}
