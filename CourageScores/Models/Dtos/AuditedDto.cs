f3747b7588f5aea03562d63f1325089deae3d28e
Simon Laing <laingster@gmail.com>
2022-12-05 12:00:54 +0000

    #53: Make DTO audited properties nullable to allow DTOs to be used with updates

--- CourageScores/Models/Dtos/AuditedDto.cs
+++ CourageScores/Models/Dtos/AuditedDto.cs
@@ -5,22 +5,22 @@ public abstract class AuditedDto : CosmosDto
     /// <summary>
     /// When was this entity created
     /// </summary>
-    public DateTime Created { get; set; }
+    public DateTime? Created { get; set; }
 
     /// <summary>
     /// Who created this entity
     /// </summary>
-    public string Author { get; set; } = null!;
+    public string? Author { get; set; }
 
     /// <summary>
     /// When was this entity last edited
     /// </summary>
-    public DateTime Updated { get; set; }
+    public DateTime? Updated { get; set; }
 
     /// <summary>
     /// Who last modified this entity
     /// </summary>
-    public string Editor { get; set; } = null!;
+    public string? Editor { get; set; }
 
     /// <summary>
     /// When was this entity deleted
