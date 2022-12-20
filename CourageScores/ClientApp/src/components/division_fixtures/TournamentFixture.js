e376a518df361fc8e3d9955860f0f7d507509100
Simon Laing <laingster@gmail.com>
2022-12-20 13:10:21 +0000

    #106: Fix display of tournament winner

--- CourageScores/ClientApp/src/components/division_fixtures/TournamentFixture.js
+++ CourageScores/ClientApp/src/components/division_fixtures/TournamentFixture.js
@@ -78,7 +78,7 @@ export function TournamentFixture({ account, tournament, onTournamentChanged, se
 
         if (round && round.matches && round.matches.length === 1) {
             const match = round.matches[0];
-            if (match.scoreA && match.scoreB && match.sideA && match.sideB) {
+            if (match.scoreA !== null && match.scoreB !== null && match.sideA && match.sideB) {
                 if (Number.parseInt(match.scoreA) > Number.parseInt(match.scoreB)) {
                     return renderWiningSide(match.sideA, rounds);
                 } else if (Number.parseInt(match.scoreB) > Number.parseInt(match.scoreA)) {
