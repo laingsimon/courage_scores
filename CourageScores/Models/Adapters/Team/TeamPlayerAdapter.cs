be96d4642f04b58fd134bd18fb40a15e5d521c6b
Simon Laing <laingster@gmail.com>
2022-12-23 14:18:01 +0000

    #116: Ensure email addresses are not accidentally exposed

--- CourageScores/Models/Adapters/Team/TeamPlayerAdapter.cs
+++ CourageScores/Models/Adapters/Team/TeamPlayerAdapter.cs
@@ -1,19 +1,31 @@
 ﻿using CourageScores.Models.Cosmos.Team;
 using CourageScores.Models.Dtos.Team;
+using CourageScores.Services.Identity;
 
 namespace CourageScores.Models.Adapters.Team;
 
 public class TeamPlayerAdapter : IAdapter<TeamPlayer, TeamPlayerDto>
 {
-    public Task<TeamPlayerDto> Adapt(TeamPlayer model)
+    private readonly IUserService _userService;
+
+    public TeamPlayerAdapter(IUserService userService)
     {
-        return Task.FromResult(new TeamPlayerDto
+        _userService = userService;
+    }
+
+    public async Task<TeamPlayerDto> Adapt(TeamPlayer model)
+    {
+        var canShowEmailAddress = await CanShowEmailAddress(model);
+
+        return new TeamPlayerDto
         {
             Captain = model.Captain,
             Id = model.Id,
             Name = model.Name,
-            EmailAddress = model.EmailAddress,
-        }.AddAuditProperties(model));
+            EmailAddress = canShowEmailAddress
+                ? model.EmailAddress
+                : null,
+        }.AddAuditProperties(model);
     }
 
     public Task<TeamPlayer> Adapt(TeamPlayerDto dto)
@@ -25,5 +37,18 @@ public class TeamPlayerAdapter : IAdapter<TeamPlayer, TeamPlayerDto>
             Name = dto.Name.Trim(),
             EmailAddress = dto.EmailAddress?.Trim(),
         }.AddAuditProperties(dto));
+    }
+
+    private async Task<bool> CanShowEmailAddress(TeamPlayer player)
+    {
+        var user = await _userService.GetUser();
+        if (user == null)
+        {
+            return false;
+        }
+
+        return user.Access?.ManageAccess == true
+               || user.Access?.ManageTeams == true
+               || player.EmailAddress == user.EmailAddress;
     }
 }
