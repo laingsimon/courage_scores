cbfa44e5886ab1eb2d5c647e8130e1464aba6e0c
Simon Laing <laingster@gmail.com>
2023-01-04 08:07:22 +0000

    #103: Add tests for UpdatePlayerCommand

--- CourageScores/Services/Command/UpdatePlayerCommand.cs
+++ CourageScores/Services/Command/UpdatePlayerCommand.cs
@@ -12,17 +12,17 @@ public class UpdatePlayerCommand : IUpdateCommand<Team, TeamPlayer>
 {
     private readonly IUserService _userService;
     private readonly ISeasonService _seasonService;
-    private readonly ISystemClock _clock;
     private readonly IGenericRepository<Models.Cosmos.Game.Game> _gameRepository;
+    private readonly IAuditingHelper _auditingHelper;
     private Guid? _playerId;
     private EditTeamPlayerDto? _player;
     private Guid? _seasonId;
 
-    public UpdatePlayerCommand(IUserService userService, ISeasonService seasonService, ISystemClock clock, IGenericRepository<Models.Cosmos.Game.Game> gameRepository)
+    public UpdatePlayerCommand(IUserService userService, ISeasonService seasonService, IAuditingHelper auditingHelper, IGenericRepository<Models.Cosmos.Game.Game> gameRepository)
     {
         _userService = userService;
         _seasonService = seasonService;
-        _clock = clock;
+        _auditingHelper = auditingHelper;
         _gameRepository = gameRepository;
     }
 
@@ -86,22 +86,21 @@ public class UpdatePlayerCommand : IUpdateCommand<Team, TeamPlayer>
         var teamSeason = model.Seasons.SingleOrDefault(s => s.SeasonId == season.Id);
         if (teamSeason == null)
         {
-            return new CommandOutcome<TeamPlayer>(false, $"Team ${model.Name} is not registered to the ${season.Name} season", null);
+            return new CommandOutcome<TeamPlayer>(false, $"Team {model.Name} is not registered to the {season.Name} season", null);
         }
 
         var player = teamSeason.Players.SingleOrDefault(p => p.Id == _playerId);
         if (player == null)
         {
-            return new CommandOutcome<TeamPlayer>(false, $"Player does not have a player with this id for the {season.Name} season", null);
+            return new CommandOutcome<TeamPlayer>(false, $"Team does not have a player with this id for the {season.Name} season", null);
         }
 
         var updatedGames = await UpdateGames(token).CountAsync();
 
         player.Name = _player.Name;
         player.Captain = _player.Captain;
-        player.Updated = _clock.UtcNow.UtcDateTime;
-        player.Editor = user.Name;
         player.EmailAddress = _player.EmailAddress ?? player.EmailAddress;
+        await _auditingHelper.SetUpdated(player, token);
         return new CommandOutcome<TeamPlayer>(true, $"Player {player.Name} updated in the {season.Name} season, {updatedGames} game/s updated", player);
     }
 
