b1805920fd1edce0849551a0eb9e99d635ebead9
Simon Laing <laingster@gmail.com>
2023-01-09 19:16:17 +0000

    #130: Fix missing react key for unfiltered properties

--- CourageScores/ClientApp/src/components/division_fixtures/FilterFixtures.js
+++ CourageScores/ClientApp/src/components/division_fixtures/FilterFixtures.js
@@ -3,17 +3,17 @@ import {nameSort} from "../../Utilities";
 
 export function FilterFixtures({ filter, setFilter, teams }) {
     const teamFilters = teams.sort(nameSort).map(t => { return { value: t.id, text: t.name }; });
-    teamFilters.unshift({ value: undefined, text: 'All teams' });
+    teamFilters.unshift({ value: null, text: 'All teams' });
 
     const typeFilters = [
-        { value: undefined,  text: 'All games' },
+        { value: null,  text: 'All games' },
         { value: 'league',  text: 'League games' },
         { value: 'knockout',  text: 'Knockouts' },
         { value: 'tournament',  text: 'Tournaments' }
     ];
 
     const dateFilters = [
-        { value: undefined, text: 'All dates' },
+        { value: null, text: 'All dates' },
         { value: 'past', text: 'Past dates' },
         { value: 'future', text: 'Future dates' },
         { value: 'last+next', text: 'Prev & next dates' },
@@ -26,8 +26,8 @@ export function FilterFixtures({ filter, setFilter, teams }) {
     }
 
     return (<div className="mb-3">
-        <BootstrapDropdown onChange={op => changeFilter('type', op)} options={typeFilters} value={filter.type} className="dynamic-width-dropdown margin-right" />
-        <BootstrapDropdown onChange={op => changeFilter('date', op)} options={dateFilters} value={filter.date} className="dynamic-width-dropdown margin-right" />
-        <BootstrapDropdown onChange={op => changeFilter('teamId', op)} options={teamFilters} value={filter.teamId} className="dynamic-width-dropdown margin-right" />
+        <BootstrapDropdown onChange={op => changeFilter('type', op)} options={typeFilters} value={filter.type || null} className="dynamic-width-dropdown margin-right" />
+        <BootstrapDropdown onChange={op => changeFilter('date', op)} options={dateFilters} value={filter.date || null} className="dynamic-width-dropdown margin-right" />
+        <BootstrapDropdown onChange={op => changeFilter('teamId', op)} options={teamFilters} value={filter.teamId || null} className="dynamic-width-dropdown margin-right" />
     </div>);
 }
