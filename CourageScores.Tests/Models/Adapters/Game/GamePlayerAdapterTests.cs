5bfd60963e4772e92e74308ab07535f7f9b8b5c9
Simon Laing <laingster@gmail.com>
2023-01-12 16:06:49 +0000

    #103: Add tests for GameAdapter, GameMatchAdapter, GamePlayerAdapter

---
+++ CourageScores.Tests/Models/Adapters/Game/GamePlayerAdapterTests.cs
@@ -0,0 +1,56 @@
+using CourageScores.Models.Adapters.Game;
+using CourageScores.Models.Cosmos.Game;
+using CourageScores.Models.Dtos.Game;
+using NUnit.Framework;
+
+namespace CourageScores.Tests.Models.Adapters.Game;
+
+[TestFixture]
+public class GamePlayerAdapterTests
+{
+    private readonly CancellationToken _token = new CancellationToken();
+    private readonly GamePlayerAdapter _adapter = new GamePlayerAdapter();
+
+    [Test]
+    public async Task Adapt_GivenModel_MapsPropertiesCorrectly()
+    {
+        var model = new GamePlayer
+        {
+            Id = Guid.NewGuid(),
+            Name = "Simon",
+        };
+
+        var result = await _adapter.Adapt(model, _token);
+
+        Assert.That(result.Id, Is.EqualTo(model.Id));
+        Assert.That(result.Name, Is.EqualTo(model.Name));
+    }
+
+    [Test]
+    public async Task Adapt_GivenDto_MapsPropertiesCorrectly()
+    {
+        var dto = new GamePlayerDto
+        {
+            Id = Guid.NewGuid(),
+            Name = "Simon",
+        };
+
+        var result = await _adapter.Adapt(dto, _token);
+
+        Assert.That(result.Id, Is.EqualTo(dto.Id));
+        Assert.That(result.Name, Is.EqualTo(dto.Name));
+    }
+
+    [Test]
+    public async Task Adapt_GivenDto_TrimsTrailingWhitespace()
+    {
+        var dto = new GamePlayerDto
+        {
+            Name = "Simon   ",
+        };
+
+        var result = await _adapter.Adapt(dto, _token);
+
+        Assert.That(result.Name, Is.EqualTo("Simon"));
+    }
+}
--- CourageScores/Models/Dtos/Game/GameMatchDto.cs
+++ CourageScores/Models/Dtos/Game/GameMatchDto.cs
@@ -22,12 +22,12 @@ public class GameMatchDto : AuditedDto
     /// <summary>
     /// Who played from the home team
     /// </summary>
-    public List<GamePlayerDto> HomePlayers { get; set; } = null!;
+    public List<GamePlayerDto> HomePlayers { get; set; } = new();
 
     /// <summary>
     /// Who played from the away team
     /// </summary>
-    public List<GamePlayerDto> AwayPlayers { get; set; } = null!;
+    public List<GamePlayerDto> AwayPlayers { get; set; } = new();
 
     /// <summary>
     /// What was the home score
@@ -42,10 +42,10 @@ public class GameMatchDto : AuditedDto
     /// <summary>
     /// Who scored a 180 in the match
     /// </summary>
-    public List<GamePlayerDto> OneEighties { get; set; } = null!;
+    public List<GamePlayerDto> OneEighties { get; set; } = new();
 
     /// <summary>
     /// Who checked out with more than 100
     /// </summary>
-    public List<NotablePlayerDto> Over100Checkouts { get; set; } = null!;
+    public List<NotablePlayerDto> Over100Checkouts { get; set; } = new();
 }
