07af97b036f2da758472293e6edf2f2d1afb3e9b
Simon Laing <laingster@gmail.com>
2023-01-16 08:06:47 +0000

    #140: Add controller to support get/add/update/delete of notes

---
+++ CourageScores/Controllers/NoteController.cs
@@ -0,0 +1,47 @@
+using CourageScores.Models.Cosmos;
+using CourageScores.Models.Dtos;
+using CourageScores.Services;
+using CourageScores.Services.Command;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourageScores.Controllers;
+
+[ApiController]
+public class NoteController : Controller
+{
+    private readonly IGenericDataService<FixtureDateNote, FixtureDateNoteDto> _noteService;
+    private readonly ICommandFactory _commandFactory;
+
+    public NoteController(
+        IGenericDataService<FixtureDateNote, FixtureDateNoteDto> noteService,
+        ICommandFactory commandFactory)
+    {
+        _noteService = noteService;
+        _commandFactory = commandFactory;
+    }
+
+    [HttpGet("/api/Note/{seasonId}")]
+    public IAsyncEnumerable<FixtureDateNoteDto> GetForSeason(Guid seasonId, CancellationToken token)
+    {
+        return _noteService.GetWhere($"t.SeasonId = '{seasonId}'", token);
+    }
+
+    [HttpPut("/api/Note/{id}")]
+    public async Task<ActionResultDto<FixtureDateNoteDto>> Upsert(Guid id, FixtureDateNoteDto note, CancellationToken token)
+    {
+        var command = _commandFactory.GetCommand<AddOrUpdateNoteCommand>().WithData(note);
+        return await _noteService.Upsert(id, command, token);
+    }
+
+    [HttpPost("/api/Note")]
+    public async Task<ActionResultDto<FixtureDateNoteDto>> Create(FixtureDateNoteDto note, CancellationToken token)
+    {
+        return await Upsert(Guid.NewGuid(), note, token);
+    }
+
+    [HttpDelete("/api/Note/{id}")]
+    public async Task<ActionResultDto<FixtureDateNoteDto>> Delete(Guid id, CancellationToken token)
+    {
+        return await _noteService.Delete(id, token);
+    }
+}
