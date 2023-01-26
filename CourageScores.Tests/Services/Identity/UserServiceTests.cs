8ad51a2c3d39ec4473f088c32469faaec4be9241
Simon Laing <laingster@gmail.com>
2023-01-26 08:05:47 +0000

    #103: Add tests for UserService

--- CourageScores.Tests/Services/Identity/UserServiceTests.cs
+++ CourageScores.Tests/Services/Identity/UserServiceTests.cs
@@ -260,4 +260,114 @@ public class UserServiceTests
         Assert.That(result!.Access, Is.Not.Null);
         Assert.That(result.Access!.ManageAccess, Is.True);
     }
+
+    [Test]
+    public async Task GetUser_GivenEmailAddressAndNotLoggedIn_ReturnsNull()
+    {
+        _httpContext = new DefaultHttpContext
+        {
+            RequestServices = _httpContextServices.Object,
+        };
+        _authenticationService
+            .Setup(s => s.AuthenticateAsync(_httpContext, CookieAuthenticationDefaults.AuthenticationScheme))
+            .ReturnsAsync(AuthenticateResult.NoResult);
+
+        var result = await _service.GetUser("someoneelse@somewhere.com", _token);
+
+        Assert.That(result, Is.Null);
+    }
+
+    [Test]
+    public async Task GetUser_GivenEmailAddressAndNotAnAdmin_ReturnsNull()
+    {
+        _httpContext = new DefaultHttpContext
+        {
+            RequestServices = _httpContextServices.Object,
+        };
+        var identity = new GenericIdentity("Simon Laing", "type");
+        identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress", "email@somewhere.com"));
+        identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname", "Simon"));
+        var ticket = new AuthenticationTicket(new ClaimsPrincipal(identity), CookieAuthenticationDefaults.AuthenticationScheme);
+        var user = new User
+        {
+            Access = new Access
+            {
+                ManageAccess = false,
+            }
+        };
+        _authenticationService
+            .Setup(s => s.AuthenticateAsync(_httpContext, CookieAuthenticationDefaults.AuthenticationScheme))
+            .ReturnsAsync(AuthenticateResult.Success(ticket));
+        _userRepository.Setup(u => u.GetUser("email@somewhere.com")).ReturnsAsync(() => user);
+        _teamRepository.Setup(r => r.GetAll(_token)).Returns(TestUtilities.AsyncEnumerable<Team>());
+
+        var result = await _service.GetUser("someoneelse@somewhere.com", _token);
+
+        Assert.That(result, Is.Null);
+        _userRepository.Verify(r => r.GetUser("someoneelse@somewhere.com"), Times.Never);
+    }
+
+    [Test]
+    public async Task GetUser_GivenEmailAddressAndAnAdminAndUserNotFound_ReturnsNull()
+    {
+        _httpContext = new DefaultHttpContext
+        {
+            RequestServices = _httpContextServices.Object,
+        };
+        var identity = new GenericIdentity("Simon Laing", "type");
+        identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress", "email@somewhere.com"));
+        identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname", "Simon"));
+        var ticket = new AuthenticationTicket(new ClaimsPrincipal(identity), CookieAuthenticationDefaults.AuthenticationScheme);
+        var user = new User
+        {
+            Access = new Access
+            {
+                ManageAccess = true,
+            }
+        };
+        _authenticationService
+            .Setup(s => s.AuthenticateAsync(_httpContext, CookieAuthenticationDefaults.AuthenticationScheme))
+            .ReturnsAsync(AuthenticateResult.Success(ticket));
+        _userRepository.Setup(u => u.GetUser("email@somewhere.com")).ReturnsAsync(() => user);
+        _userRepository.Setup(u => u.GetUser("someoneelse@somewhere.com")).ReturnsAsync(() => null);
+        _teamRepository.Setup(r => r.GetAll(_token)).Returns(TestUtilities.AsyncEnumerable<Team>());
+
+        var result = await _service.GetUser("someoneelse@somewhere.com", _token);
+
+        Assert.That(result, Is.Null);
+        _userRepository.Verify(r => r.GetUser("someoneelse@somewhere.com"));
+    }
+
+    [Test]
+    public async Task GetUser_GivenEmailAddressAndAnAdminAndUserFound_ReturnsUser()
+    {
+        _httpContext = new DefaultHttpContext
+        {
+            RequestServices = _httpContextServices.Object,
+        };
+        var identity = new GenericIdentity("Simon Laing", "type");
+        identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress", "email@somewhere.com"));
+        identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname", "Simon"));
+        var ticket = new AuthenticationTicket(new ClaimsPrincipal(identity), CookieAuthenticationDefaults.AuthenticationScheme);
+        var otherUser = new User { EmailAddress = "someoneelse@somewhere.com" };
+        var user = new User
+        {
+            Access = new Access
+            {
+                ManageAccess = true,
+            }
+        };
+        _authenticationService
+            .Setup(s => s.AuthenticateAsync(_httpContext, CookieAuthenticationDefaults.AuthenticationScheme))
+            .ReturnsAsync(AuthenticateResult.Success(ticket));
+        _userRepository.Setup(u => u.GetUser("email@somewhere.com")).ReturnsAsync(() => user);
+        _userRepository.Setup(u => u.GetUser(otherUser.EmailAddress)).ReturnsAsync(() => otherUser);
+        _teamRepository.Setup(r => r.GetAll(_token)).Returns(TestUtilities.AsyncEnumerable<Team>());
+
+        var result = await _service.GetUser(otherUser.EmailAddress, _token);
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result!.EmailAddress, Is.EqualTo(otherUser.EmailAddress));
+        _userRepository.Verify(r => r.GetUser(otherUser.EmailAddress));
+    }
 }
