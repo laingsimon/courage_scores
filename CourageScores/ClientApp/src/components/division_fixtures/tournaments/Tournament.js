1f7dcbcc08c6cc850b176c1c668b4ac06426e8e0
Simon Laing <laingster@gmail.com>
2023-02-02 12:39:22 +0000

    Add 108s and hi-check to tournaments with divisionId

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/Tournament.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/Tournament.js
@@ -220,10 +220,7 @@ export function Tournament({ account, apis }) {
             newTournamentData.oneEighties = [];
         }
 
-        newTournamentData.oneEighties.push({
-            id: player.id,
-            name: player.name
-        });
+        newTournamentData.oneEighties.push(player);
 
         setTournamentData(newTournamentData);
 
@@ -236,11 +233,7 @@ export function Tournament({ account, apis }) {
             newTournamentData.over100Checkouts = [];
         }
 
-        newTournamentData.over100Checkouts.push({
-            id: player.id,
-            name: player.name,
-            notes: notes
-        });
+        newTournamentData.over100Checkouts.push(Object.assign({}, player, { notes: notes }));
 
         setTournamentData(newTournamentData);
     }
