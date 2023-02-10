19787bc114a47273739ad99ce11695a06d4a2e7e
Simon Laing <laingster@gmail.com>
2023-02-10 07:20:06 +0000

    Present details of server side errors

--- CourageScores/ClientApp/src/components/common/ErrorDisplay.js
+++ CourageScores/ClientApp/src/components/common/ErrorDisplay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Dialog} from "./Dialog";
 
-export function ErrorDisplay({ errors, messages, warnings, onClose, title }) {
+export function ErrorDisplay({ errors, messages, warnings, onClose, title, Exception }) {
     let index = 0;
 
     function renderValidationErrors(errors) {
@@ -12,8 +12,21 @@ export function ErrorDisplay({ errors, messages, warnings, onClose, title }) {
         </ol>)
     }
 
+    function renderServerSideException(exc) {
+        const type = exc.Type;
+        const stack = exc.StackTrace;
+        const message = exc.Message;
+
+        return (<div>
+            <h5>Server side error</h5>
+            <p><strong>{type}</strong>: {message}</p>
+            <pre>{stack.join('\n')}</pre>
+        </div>);
+    }
+
     return (<Dialog onClose={onClose} title={title || 'There was an error'}>
         <div>
+             {Exception ? renderServerSideException(Exception) : null}
              {errors && errors.length ? errors.map(e => (<p key={index++} className="text-danger">{e}</p>)) : null}
              {errors && !errors.length ? (renderValidationErrors(errors)): null}
              {warnings ? warnings.map(w => (<p key={index++} className="text-warning">{w}</p>)) : null}
