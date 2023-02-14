f5ec520301745d7d550649820ab5bb54905c2445
Simon Laing <laingster@gmail.com>
2023-02-14 07:59:48 +0000

    #146: Show space for 180s and hi-checks to be recorded

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/TournamentSheet.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/TournamentSheet.js
@@ -2,6 +2,14 @@ import {sortBy} from "../../../Utilities";
 import React from "react";
 
 export function TournamentSheet({ sides }) {
+    const maxSideSize = sides.reduce((prev, current) => {
+        if (current.players.length > prev) {
+            return current.players.length;
+        }
+
+        return prev;
+    }, 0);
+
     function repeat(count) {
         const items = [];
 
@@ -38,8 +46,16 @@ export function TournamentSheet({ sides }) {
         if (noOfMatches + byes < 2) {
             return (<div className="d-flex flex-row m-2 align-items-center">
                 <div className="d-flex flex-column m-2">
-                    <div className="text-center fw-bold">Venue winner</div>
+                    <div className="mb-5">
+                        <div className="text-center fw-bold text-secondary">180s</div>
+                        <div className="outline-dark outline-dashed min-width-200 min-height-100"></div>
+                    </div>
+                    <div className="text-center fw-bold text-primary">Venue winner</div>
                     <div className="outline-dark m-2 min-width-150 min-height-50"></div>
+                    <div className="mt-5">
+                        <div className="text-center fw-bold text-secondary">Hi checks</div>
+                        <div className="outline-dark outline-dashed min-width-200 min-height-100"></div>
+                    </div>
                 </div>
             </div>);
         }
@@ -47,7 +63,9 @@ export function TournamentSheet({ sides }) {
         return (<div className="d-flex flex-row m-2 align-items-center">
             <div className="d-flex flex-column m-2">
                 <div className="text-center fw-bold">{getRoundName(noOfMatches + byes, depth)}</div>
-                {repeat(noOfMatches).map(index => (<div key={index} className="outline-dark m-2 min-width-150 min-height-50"><hr className="my-5 border-dark border-bottom-0 opacity-100" /></div>))}
+                {repeat(noOfMatches).map(index => (<div key={index} className="outline-dark m-2 min-width-150 min-height-50">
+                    {repeat(maxSideSize - 1).map(playerIndex => (<hr key={playerIndex} className="my-5 border-dark border-bottom-0 opacity-100" />))}
+                </div>))}
                 {byes ? (<div className="outline-dark m-2 min-width-150 min-height-50 bg-light-warning outline-dashed">
                     <span className="float-end px-2 small">Bye</span>
                 </div>) : null}
@@ -63,7 +81,7 @@ export function TournamentSheet({ sides }) {
             {renderPrintModeRound(sides.length, 0)}
             <ul className="float-end list-group">{sides
                 .sort(sortBy('name'))
-                .map(s => (<li className="list-group-item my-2 outline-dark py-2" key={s.id}>{++index} - {s.name}</li>))}</ul>
+                .map(s => (<li className="list-group-item outline-dark" key={s.id}>{++index} - {s.name}</li>))}</ul>
         </div>
     </div>);
 }
