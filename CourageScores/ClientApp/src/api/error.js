dd2fa90bea9f66d0ec76b28fb3ef1c3c74358629
Simon Laing <laingsimon@users.noreply.github.com>
2023-02-20 07:56:38 +0000

    #195: add client error api

---
+++ CourageScores/ClientApp/src/api/error.js
@@ -0,0 +1,19 @@
+class ErrorApi {
+    constructor(http) {
+        this.http = http;
+    }
+
+    get(id) {
+        return this.http.get(`/api/Error/${emailAddress}`, {});
+    }
+
+    getRecent(since) {
+        return this.http.get(`/api/Error/Since/${since}`, {});
+    }
+
+    add(error) {
+        return this.http.put(`/api/Error`, error);
+    }
+}
+
+export { ErrorApi };
