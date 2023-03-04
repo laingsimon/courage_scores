5e2ef4346f6d1c373c62f73c465f22abdee6da3f
Simon Laing <laingster@gmail.com>
2023-03-04 09:45:25 +0000

    #207: Update match options when updating tournament rounds

--- CourageScores.Tests/Services/Command/UpdateTournamentRoundsCommandTests.cs
+++ CourageScores.Tests/Services/Command/UpdateTournamentRoundsCommandTests.cs
@@ -1,8 +1,10 @@
 ﻿using CourageScores.Filters;
+using CourageScores.Models.Adapters;
 using CourageScores.Models.Adapters.Game;
 using CourageScores.Models.Cosmos.Game;
 using CourageScores.Models.Dtos.Game;
 using CourageScores.Services.Command;
+using CourageScores.Tests.Models.Adapters;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
@@ -16,6 +18,9 @@ public class UpdateTournamentRoundsCommandTests
     private readonly CancellationToken _token = new CancellationToken();
     private TournamentRoundDto _update = null!;
     private ScopedCacheManagementFlags _cacheFlags = null!;
+    private ISimpleAdapter<GameMatchOption?, GameMatchOptionDto?> _matchOptionAdapter = null!;
+    private GameMatchOption? _matchOption;
+    private GameMatchOptionDto? _matchOptionDto;
 
     [SetUp]
     public void SetupEachTest()
@@ -24,10 +29,14 @@ public class UpdateTournamentRoundsCommandTests
         _game = new TournamentGame();
         _update = new TournamentRoundDto();
         _cacheFlags = new ScopedCacheManagementFlags();
+        _matchOption = new GameMatchOption();
+        _matchOptionDto = new GameMatchOptionDto();
+        _matchOptionAdapter = new MockSimpleAdapter<GameMatchOption?, GameMatchOptionDto?>(_matchOption, _matchOptionDto);
         _command = new UpdateTournamentRoundsCommand(
             sideAdapter,
             new TournamentMatchAdapter(sideAdapter),
-            _cacheFlags);
+            _cacheFlags,
+            _matchOptionAdapter);
     }
 
     [Test]
@@ -126,6 +135,10 @@ public class UpdateTournamentRoundsCommandTests
                     ScoreB = 2,
                     Id = Guid.NewGuid(),
                 }
+            },
+            MatchOptions =
+            {
+                _matchOptionDto,
             }
         };
         _update.NextRound = round2;
@@ -186,7 +199,8 @@ public class UpdateTournamentRoundsCommandTests
             return actual.Name == update.Name
                 && IsEqual(actual.Sides, update.Sides)
                 && IsEqual(actual.Matches, update.Matches)
-                && (actual.NextRound == null) == (update.NextRound == null);
+                && (actual.NextRound == null) == (update.NextRound == null)
+                && IsEqual(actual.MatchOptions, update.MatchOptions);
         }
 
         private static bool IsEqual(IReadOnlyCollection<TournamentSide> sides, IReadOnlyCollection<TournamentSideDto> updateSides)
@@ -200,6 +214,19 @@ public class UpdateTournamentRoundsCommandTests
             return side.Id == updateSide.Id
                    && side.Name == updateSide.Name
                    && IsEqual(side.Players, updateSide.Players);
+        }
+
+        private static bool IsEqual(IReadOnlyCollection<GameMatchOption?> matchOptions, IReadOnlyCollection<GameMatchOptionDto?> updateMatchOptions)
+        {
+            return matchOptions.Count == updateMatchOptions.Count
+                   && matchOptions.Zip(updateMatchOptions, IsEqual).All(doesMatch => doesMatch);
+        }
+
+        private static bool IsEqual(GameMatchOption? matchOption, GameMatchOptionDto? updateMatchOption)
+        {
+            return matchOption?.PlayerCount == updateMatchOption?.PlayerCount
+                   && matchOption?.StartingScore == updateMatchOption?.StartingScore
+                   && matchOption?.NumberOfLegs == updateMatchOption?.NumberOfLegs;
         }
 
         private static bool IsEqual(IReadOnlyCollection<TournamentPlayer> players, IReadOnlyCollection<TournamentPlayerDto> updatePlayers)
