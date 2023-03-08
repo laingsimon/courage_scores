bf74b46a93fa2c0256230ff77b2ab53d9894e701
Simon Laing <laingster@gmail.com>
2023-03-08 20:10:52 +0000

    #216: Add ability to have ad-hoc scoring

--- CourageScores/ClientApp/src/components/SelfScore.js
+++ CourageScores/ClientApp/src/components/SelfScore.js
@@ -6,13 +6,24 @@ export function SelfScore() {
     const [ startingScore, setStartingScore ] = useState('501');
     const [ numberOfLegs, setNumberOfLegs ] = useState('3');
     const [ homeScore, setHomeScore ] = useState(0);
+    const [ awayScore, setAwayScore ] = useState(0);
     const [ data, setData ] = useState(null);
+    const [ yourName, setYourName ] = useState('you');
+    const [ opponentName, setOpponentName ] = useState('');
 
     function restart() {
         setData({
             legs: {}
         });
         setHomeScore(0);
+        setAwayScore(0);
+    }
+
+    function onOtherNameChange(event) {
+        setOpponentName(event.target.value);
+        setData(null);
+        setHomeScore(0);
+        setAwayScore(0);
     }
 
     return (<div className="p-3 light-background">
@@ -25,6 +36,16 @@ export function SelfScore() {
                 <span className="input-group-text">Starting score</span>
             </div>
             <input type="number" className="form-control" value={startingScore} onChange={stateChanged(setStartingScore)} />
+        </div>
+        <div className="input-group my-3">
+            <div className="input-group-prepend">
+                <span className="input-group-text">Your name</span>
+            </div>
+            <input className="form-control" value={yourName} onChange={stateChanged(setYourName)} />
+            <div className="input-group-prepend">
+                <span className="input-group-text">Opponent</span>
+            </div>
+            <input placeholder="Optional" className="form-control" value={opponentName} onChange={onOtherNameChange} />
             <button className="btn btn-primary" onClick={restart}>{homeScore > 0 ? 'Restart...' : 'Start...'}</button>
         </div>
         {data != null ? (<ScoreAsYouGo
@@ -34,13 +55,14 @@ export function SelfScore() {
             on180={() => {}}
             onChange={(newData) => setData(newData)}
             homeScore={homeScore}
-            awayScore={0}
-            away="No-one"
-            home="You"
+            awayScore={awayScore}
+            away={opponentName}
+            home={yourName}
             data={data}
-            singlePlayer={true}
-            onLegComplete={(homeScore, _) => {
+            singlePlayer={opponentName === ''}
+            onLegComplete={(homeScore, awayScore) => {
                 setHomeScore(homeScore);
+                setAwayScore(awayScore);
             }} />) : null}
     </div>)
 }
