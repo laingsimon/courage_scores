9eb1389107745baf32870ff4557706f4bdab1764
Simon Laing <laingster@gmail.com>
2023-03-22 08:00:52 +0000

    Dont offer to show the stack if there isn't one

--- CourageScores/ClientApp/src/components/PageError.js
+++ CourageScores/ClientApp/src/components/PageError.js
@@ -40,10 +40,10 @@ export function PageError({ error }){
         <h3 className="text-danger">An error occurred</h3>
         <p>
             <span className="fw-bold">{error.message || error}</span>
-            <span className="form-switch margin-left">
-                    <input className="form-check-input" type="checkbox" id="showStack" checked={showStack} onChange={event => setShowStack(event.target.checked)}/>
-                    <label className="margin-left form-check-label" htmlFor="showStack">Show stack</label>
-                </span>
+            {error.stack ? (<span className="form-switch margin-left">
+                <input className="form-check-input" type="checkbox" id="showStack" checked={showStack} onChange={event => setShowStack(event.target.checked)}/>
+                <label className="margin-left form-check-label" htmlFor="showStack">Show stack</label>
+            </span>) : null}
         </p>
         {showStack && error.stack ? (<pre>{error.stack}</pre>) : null}
         <button className="btn btn-warning" onClick={clearError}>Clear error</button>
