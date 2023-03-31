24df4ba3a2a66e21770566929fcde2d4ae890fab
Simon Laing <laingsimon@users.noreply.github.com>
2023-03-31 09:10:08 -0400

    #243: remove seasons property

--- CourageScores/Models/Dtos/Division/DivisionDataDto.cs
+++ CourageScores/Models/Dtos/Division/DivisionDataDto.cs
@@ -13,7 +13,7 @@ public class DivisionDataDto
     public List<DivisionFixtureDateDto> Fixtures { get; set; } = new();
     public List<DivisionPlayerDto> Players { get; set; } = new();
     public DivisionDataSeasonDto? Season { get; set; }
-    public List<DivisionDataSeasonDto> Seasons { get; set; } = new();
+
     [JsonPropertyName("allTeams")]
     public List<DivisionTeamDetailsDto> TeamsInSeason { get; set; } = new();
 }
