ec74a435dfa6bf32a69f531193008d0d1f43e8db
Simon Laing <laingsimon@users.noreply.github.com>
2023-04-03 16:50:33 +0100

    updste tests

--- CourageScores.Tests/Services/Division/DivisionDataDtoFactoryTests.cs
+++ CourageScores.Tests/Services/Division/DivisionDataDtoFactoryTests.cs
@@ -36,6 +36,7 @@ public class DivisionDataDtoFactoryTests
         _divisionDataSeasonAdapter = new DivisionDataSeasonAdapter();
         _divisionFixtureDateAdapter = new Mock<IDivisionFixtureDateAdapter>();
         _userService = new Mock<IUserService>();
+        _user = null;
 
         _factory = new DivisionDataDtoFactory(
             _divisionPlayerAdapter,
@@ -272,6 +273,49 @@ public class DivisionDataDtoFactoryTests
     [Test]
     public async Task CreateDivisionDataDto_GivenDataErrorsWhenNotLoggedIn_SetsDataErrorsToEmpty()
     {
+        var team1 = new TeamDto { Id = Guid.NewGuid(), Name = "Team 1 - Playing" };
+        var team2 = new TeamDto { Id = Guid.NewGuid(), Name = "Team 2 - Playing" };
+        var game = new CosmosGame
+        {
+            Date = new DateTime(2001, 02, 03),
+            Id = Guid.NewGuid(),
+            Home = new GameTeam { Id = team1.Id },
+            Away = new GameTeam { Id = team2.Id },
+            Matches =
+            {
+                new GameMatch
+                {
+                    HomeScore = 2,
+                    AwayScore = 3,
+                    HomePlayers = { new GamePlayer { Id = Guid.NewGuid(), Name = "A" } },
+                    AwayPlayers = { new GamePlayer { Id = Guid.NewGuid(), Name = "B" }, new GamePlayer { Id = Guid.NewGuid(), Name = "C" } },
+                },
+            },
+        };
+        var context = new DivisionDataContext(
+            new[] { game },
+            new List<TeamDto> { team1, team2 },
+            new List<TeamDto> { team1, team2 },
+            Array.Empty<TournamentGame>(),
+            Array.Empty<FixtureDateNoteDto>(),
+            new SeasonDto(),
+            Array.Empty<SeasonDto>());
+
+        var result = await _factory.CreateDivisionDataDto(context, null, _token);
+
+        Assert.That(result.DataErrors, Is.Empty);
+    }
+
+    [Test]
+    public async Task CreateDivisionDataDto_GivenDataErrors_SetsDataErrorsCorrectly()
+    {
+        _user = new UserDto
+        {
+            Access = new AccessDto
+            {
+                ImportData = false,
+            }
+        };
         var team1 = new TeamDto { Id = Guid.NewGuid(), Name = "Team 1 - Playing" };
         var team2 = new TeamDto { Id = Guid.NewGuid(), Name = "Team 2 - Playing" };
         var game = new CosmosGame
