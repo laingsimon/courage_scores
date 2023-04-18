5135abf09be63d1b13d755e57cad5847514d0043
Simon Laing <laingster@gmail.com>
2023-04-18 11:23:50 +0100

    #284: Add preview to editing notes

--- CourageScores/ClientApp/src/components/division_fixtures/EditNote.js
+++ CourageScores/ClientApp/src/components/division_fixtures/EditNote.js
@@ -4,6 +4,8 @@ import {ErrorDisplay} from "../common/ErrorDisplay";
 import {valueChanged} from "../../Utilities";
 import {useDependencies} from "../../IocContainer";
 import {useApp} from "../../AppContainer";
+import ReactMarkdown from "react-markdown";
+import {FixtureDateNote} from "./FixtureDateNote";
 
 export function EditNote({ note, onNoteChanged, onClose, onSaved }) {
     const [savingNote, setSavingNote] = useState(false);
@@ -54,6 +56,12 @@ export function EditNote({ note, onNoteChanged, onClose, onSaved }) {
             <div className="form-group my-3 d-flex">
                 <label htmlFor="note-text" className="input-group-text">Note</label>
                 <textarea cols="75" rows="2" id="note-text" value={note.note} name="note" onChange={valueChanged(note, onNoteChanged)}></textarea>
+            </div>
+            <div className="form-group my-3">
+                <h5>Preview</h5>
+                <div>
+                    <FixtureDateNote note={ note } preventDelete={true} />
+                </div>
             </div>
             <div className="input-group my-3">
                 <div className="input-group-prepend">
