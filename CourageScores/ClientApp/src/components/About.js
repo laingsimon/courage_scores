e29eaa1d241b2299352f5fdd17de13582181e5b4
Simon Laing <laingster@gmail.com>
2023-04-26 19:42:48 +0100

    #314: Update About content

--- CourageScores/ClientApp/src/components/About.js
+++ CourageScores/ClientApp/src/components/About.js
@@ -2,6 +2,7 @@ import {useApp} from "../AppContainer";
 
 export function About() {
     const { build } = useApp();
+    const dateTime = new Date(build.date);
 
     return (<div className="light-background p-3">
         <h3>About</h3>
@@ -10,21 +11,21 @@ export function About() {
         </p>
 
         <p>
-            Custodians: Dave Jewell, Jon Green
+            <strong>Custodians</strong>: Dave Jewell, Jon Green
         </p>
 
         <p>
-            Author & Maintainer: <a target="_blank" rel="noreferrer" href="https://github.com/laingsimon">Simon Laing ↗</a>
+            <strong>Author & Maintainer</strong>: <a target="_blank" rel="noreferrer" href="https://github.com/laingsimon">Simon Laing ↗</a>
         </p>
 
         <p>
-            License: <a target="_blank" rel="noreferrer" href="https://github.com/laingsimon/courage_scores/blob/main/LICENSE">Apache License 2.0</a>
+            <strong>License</strong>: <a target="_blank" rel="noreferrer" href="https://github.com/laingsimon/courage_scores/blob/main/LICENSE">Apache License 2.0</a>
         </p>
 
         <table className="table">
             <thead>
             <tr>
-                <th colSpan="2">Version</th>
+                <th colSpan="2">Version Information</th>
             </tr>
             </thead>
             <tbody>
@@ -40,7 +41,7 @@ export function About() {
             </tr>
             <tr>
                 <th>Date</th>
-                <td>{build.date}</td>
+                <td title={build.date}>{dateTime.toLocaleDateString()} {dateTime.toLocaleTimeString()}</td>
             </tr>
             </tbody>
         </table>
