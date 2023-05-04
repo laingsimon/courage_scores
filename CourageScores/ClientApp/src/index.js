f19f5e69a5d12eb43d97575868b4543e15dcce8d
Simon Laing <laingster@gmail.com>
2023-05-04 07:31:17 +0100

    #300: Exclude files from coverage

--- CourageScores/ClientApp/src/index.js
+++ CourageScores/ClientApp/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import {IocContainer} from "./IocContainer";
 
+/* istanbul ignore file */
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
