eb8b275b261931171bb910b0a564535831c667d8
Simon Laing <laingster@gmail.com>
2023-06-10 13:22:26 +0100

    #369: Add models to support patching of tournaments

---
+++ CourageScores/Models/Dtos/Game/PatchTournamentMatchDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CourageScores.Models.Dtos.Game;
+
+/// <summary>
+/// Update portions of a tournament match
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class PatchTournamentMatchDto
+{
+    // identifiers for the match within the round
+    public Guid SideA { get; set; }
+    public Guid SideB { get; set; }
+
+    public int ScoreA { get; set; }
+    public int ScoreB { get; set; }
+}
