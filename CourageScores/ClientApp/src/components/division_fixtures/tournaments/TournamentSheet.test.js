b5bc774c965816b45b73a7754b964f4b134fb42d
Simon Laing <laingster@gmail.com>
2023-06-16 13:39:46 +0100

    #420: Refactor unit tests

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/TournamentSheet.test.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/TournamentSheet.test.js
@@ -99,7 +99,7 @@ describe('TournamentSheet', () => {
 
             expect(reportedError).toBeNull();
             assertRoundNames(['Final'], true);
-            assertSideNames([ 'PLAYER 1', 'PLAYER 1' ]);
+            assertSideNames(repeat(2, _ => 'PLAYER 1'));
             assertMatches(1);
             assertByes([ false ]);
         });
@@ -114,7 +114,7 @@ describe('TournamentSheet', () => {
 
             expect(reportedError).toBeNull();
             assertRoundNames(['Final']);
-            assertSideNames([ 'PLAYER 1, PLAYER 2', 'PLAYER 1, PLAYER 2' ]);
+            assertSideNames(repeat(2, _ => 'PLAYER 1, PLAYER 2'));
             assertMatches(2);
             assertByes([ false ]);
         });
@@ -130,7 +130,7 @@ describe('TournamentSheet', () => {
 
             expect(reportedError).toBeNull();
             assertRoundNames(['Semi-Final', 'Final']);
-            assertSideNames(['PLAYER 1, PLAYER 2, PLAYER 3', 'PLAYER 1, PLAYER 2, PLAYER 3', 'PLAYER 1, PLAYER 2, PLAYER 3']);
+            assertSideNames(repeat(3, _ => 'PLAYER 1, PLAYER 2, PLAYER 3'));
             assertMatches(3);
             assertByes([ true, false ]);
         });
@@ -147,7 +147,7 @@ describe('TournamentSheet', () => {
 
             expect(reportedError).toBeNull();
             assertRoundNames(['Semi-Final', 'Final']);
-            assertSideNames([ 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4', 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4', 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4', 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4' ]);
+            assertSideNames(repeat(4, _ => 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4'));
             assertMatches(4);
             assertByes([ false, false ]);
         });
@@ -165,7 +165,7 @@ describe('TournamentSheet', () => {
 
             expect(reportedError).toBeNull();
             assertRoundNames(['Quarter-Final', 'Semi-Final', 'Final']);
-            assertSideNames([ 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4, PLAYER 5', 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4, PLAYER 5', 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4, PLAYER 5', 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4, PLAYER 5', 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4, PLAYER 5' ]);
+            assertSideNames(repeat(5, _=> 'PLAYER 1, PLAYER 2, PLAYER 3, PLAYER 4, PLAYER 5'));
             assertMatches(5);
             assertByes([ true, true, false ]);
         });
@@ -187,7 +187,7 @@ describe('TournamentSheet', () => {
 
             expect(reportedError).toBeNull();
             assertRoundNames(['Round: 1', 'Quarter-Final', 'Semi-Final', 'Final'], true);
-            assertSideNames([ 'PLAYER 1', 'PLAYER 1', 'PLAYER 1', 'PLAYER 1', 'PLAYER 1', 'PLAYER 1', 'PLAYER 1', 'PLAYER 1', 'PLAYER 1' ]);
+            assertSideNames(repeat(9, _ => 'PLAYER 1'));
             assertMatches(1);
             assertByes([ true, true, true, false ]);
         });
