76da125bac4745b60208bd899550b1679c4d17b4
Simon Laing <laingster@gmail.com>
2023-06-23 20:31:03 +0100

    #327: Add initial display of summary

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/Summary.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/Summary.js
@@ -1,3 +1,55 @@
-export function Summary({ tournamentData, fixture }) {
-    return (<div className="page-break-after">SUMMARY</div>);
+import {SummaryDataRow} from "./SummaryDataRow";
+
+export function Summary({ tournamentData, fixture, saygDataMap }) {
+    const round = tournamentData.round || {};
+    const matches = round.matches || [];
+
+    return (<div className="page-break-after">
+        <h2>Summary</h2>
+        <table className="table">
+            <thead>
+            <tr>
+                <th>Match no</th>
+                <th>{fixture.home}<br />Player</th>
+                <th>Legs won</th>
+                <th>Total tons</th>
+                <th>100+</th>
+                <th>140+</th>
+                <th>180</th>
+                <th>Player average</th>
+                <th>{fixture.away}<br />Player</th>
+                <th>Legs won</th>
+                <th>Total tons</th>
+                <th>100+</th>
+                <th>140+</th>
+                <th>180</th>
+                <th>Player average</th>
+            </tr>
+            </thead>
+            <tbody>
+            {matches.map((m, index) => (<SummaryDataRow
+                saygData={saygDataMap[m.saygId]}
+                match={m}
+                matchNo={index + 1}/>))}
+            </tbody>
+            <tfoot>
+            <tr>
+                <td colSpan="2"></td>
+                <td colSpan="5" className="text-end">Rounded average</td>
+                <td></td>
+                <td colSpan="1"></td>
+                <td colSpan="5" className="text-end">Rounded average</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colSpan="2"></td>
+                <td colSpan="5" className="text-end">Darts for windows average</td>
+                <td></td>
+                <td colSpan="1"></td>
+                <td colSpan="5" className="text-end">Darts for windows average</td>
+                <td></td>
+            </tr>
+            </tfoot>
+        </table>
+    </div>);
 }
---
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/SummaryDataRow.js
@@ -0,0 +1,50 @@
+import {count, sum} from "../../../../helpers/collections";
+
+export function SummaryDataRow({ matchNo, match, saygData }) {
+    function countThrowsBetween(accumulatorName, lowerInclusive, upperExclusive) {
+        if (!saygData || !saygData.legs) {
+            return null;
+        }
+
+        return sum(Object.keys(saygData.legs).map(legIndex => {
+            const leg = saygData.legs[legIndex];
+            const accumulator = leg[accumulatorName];
+
+            return count(accumulator.throws, thr => thr.score >= lowerInclusive && thr.score < upperExclusive);
+        }));
+    }
+
+    function count100(accumulatorName) {
+        return countThrowsBetween(accumulatorName, 100, 140);
+    }
+
+    function count140(accumulatorName) {
+        return countThrowsBetween(accumulatorName, 140, 180);
+    }
+
+    function count180(accumulatorName) {
+        return countThrowsBetween(accumulatorName, 180, 181);
+    }
+
+    function countTons(accumulatorName) {
+        return count100(accumulatorName) + count140(accumulatorName) + (count180(accumulatorName) * 2);
+    }
+
+    return (<tr>
+        <td>{matchNo}</td>
+        <td>{match.sideA.name}</td>
+        <td>{match.scoreA}</td>
+        <td>{countTons('home')}</td>
+        <td>{count100('home')}</td>
+        <td>{count140('home')}</td>
+        <td>{count180('home')}</td>
+        <td></td>
+        <td>{match.sideB.name}</td>
+        <td>{match.scoreB}</td>
+        <td>{countTons('away')}</td>
+        <td>{count100('away')}</td>
+        <td>{count140('away')}</td>
+        <td>{count180('away')}</td>
+        <td></td>
+    </tr>);
+}
