53f2d2f5e9b46e39e1cc6a47066a68e2908b4854
Simon Laing <laingster@gmail.com>
2023-06-24 07:56:35 +0100

    #327: Flex the number of throws (dont always show 50)

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchLog.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchLog.js
@@ -2,9 +2,31 @@ import {MatchLogRow} from "./MatchLogRow";
 import {MatchLogTableHeading} from "./MatchLogTableHeading";
 
 export function MatchLog({ tournamentData, fixture, saygDataMap }) {
-    const noOfThrows = 50;
     const round = tournamentData.round || {};
     const matches = round.matches || [];
+    const noOfThrows = getNoOfThrows() + 1;
+
+    function getNoOfThrows() {
+        let throws = 0;
+
+        for (let index = 0; index < matches.length; index++) {
+            const match = matches[index];
+            const saygData = saygDataMap[match.saygId];
+
+            if (!saygData) {
+                continue;
+            }
+
+            for (const legsKey in saygData.legs) {
+                const leg = saygData.legs[legsKey];
+
+                throws = Math.max(throws, leg.home.throws.length);
+                throws = Math.max(throws, leg.away.throws.length);
+            }
+        }
+
+        return throws;
+    }
 
     return (<div className="page-break-after">
         <h2>Match log</h2>
