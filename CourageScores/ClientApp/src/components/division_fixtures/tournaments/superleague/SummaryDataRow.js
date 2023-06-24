3219e58894408a12b869f70d69d058dbaed5b920
Simon Laing <laingster@gmail.com>
2023-06-24 20:04:02 +0100

    #327: Generalise summary methods

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/SummaryDataRow.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/SummaryDataRow.js
@@ -1,55 +1,31 @@
-import {count, sum} from "../../../../helpers/collections";
 import {useApp} from "../../../../AppContainer";
 import {round2dp} from "../../../../helpers/rendering";
-import {getPlayerOverallAverage} from "../../../../helpers/superleague";
+import {
+    count100,
+    count140, count180,
+    countTons,
+    getPlayerOverallAverage
+} from "../../../../helpers/superleague";
 
 export function SummaryDataRow({ matchNo, match, saygData }) {
     const { onError } = useApp();
-    function countThrowsBetween(accumulatorName, lowerInclusive, upperExclusive) {
-        if (!saygData || !saygData.legs) {
-            return null;
-        }
-
-        return sum(Object.keys(saygData.legs).map(legIndex => {
-            const leg = saygData.legs[legIndex];
-            const accumulator = leg[accumulatorName];
-
-            return count(accumulator.throws, thr => thr.score >= lowerInclusive && thr.score < upperExclusive);
-        }));
-    }
-
-    function count100(accumulatorName) {
-        return countThrowsBetween(accumulatorName, 100, 140);
-    }
-
-    function count140(accumulatorName) {
-        return countThrowsBetween(accumulatorName, 140, 180);
-    }
-
-    function count180(accumulatorName) {
-        return countThrowsBetween(accumulatorName, 180, 181);
-    }
-
-    function countTons(accumulatorName) {
-        return count100(accumulatorName) + count140(accumulatorName) + (count180(accumulatorName) * 2);
-    }
 
     try {
         return (<tr>
             <td>{matchNo}</td>
             <td>{match.sideA.name}</td>
             <td>{match.scoreA}</td>
-            <td>{countTons('home')}</td>
-            <td>{count100('home')}</td>
-            <td>{count140('home')}</td>
-            <td>{count180('home')}</td>
+            <td>{countTons(saygData, 'home')}</td>
+            <td>{count100(saygData, 'home')}</td>
+            <td>{count140(saygData, 'home')}</td>
+            <td>{count180(saygData, 'home')}</td>
             <td>{round2dp(getPlayerOverallAverage(saygData, 'home'))}</td>
             <td>{match.sideB.name}</td>
             <td>{match.scoreB}</td>
-            <td>{countTons('away')}</td>
-            <td>{count100('away')}</td>
-            <td>{count140('away')}</td>
-            <td>{count180('away')}</td>
+            <td>{countTons(saygData, 'away')}</td>
+            <td>{count100(saygData, 'away')}</td>
+            <td>{count140(saygData, 'away')}</td>
+            <td>{count180(saygData, 'away')}</td>
             <td>{round2dp(getPlayerOverallAverage(saygData, 'away'))}</td>
         </tr>);
     } catch (e) {
--- CourageScores/ClientApp/src/helpers/superleague.js
+++ CourageScores/ClientApp/src/helpers/superleague.js
@@ -1,4 +1,4 @@
-import {sum} from "./collections";
+import {count, sum} from "./collections";
 
 export function getPlayerOverallAverage(saygData, sideName) {
     const metrics = Object.keys(saygData.legs).map(legIndex => {
@@ -13,3 +13,35 @@ export function getPlayerOverallAverage(saygData, sideName) {
 
     return sum(metrics, m => m.score) / sum(metrics, m => m.noOfDarts);
 }
+
+export function countThrowsBetween(saygData, accumulatorName, lowerInclusive, upperExclusive) {
+    if (!saygData || !saygData.legs) {
+        return null;
+    }
+
+    return sum(Object.keys(saygData.legs).map(legIndex => {
+        const leg = saygData.legs[legIndex];
+        const accumulator = leg[accumulatorName];
+
+        return count(
+            accumulator.throws,
+            thr => thr.score >= lowerInclusive && (!upperExclusive || thr.score < upperExclusive));
+    }));
+}
+
+export function count100(saygData, accumulatorName) {
+    return countThrowsBetween(saygData, accumulatorName, 100, 140);
+}
+
+export function count140(saygData, accumulatorName) {
+    return countThrowsBetween(saygData, accumulatorName, 140, 180);
+}
+
+export function count180(saygData, accumulatorName) {
+    return countThrowsBetween(saygData, accumulatorName, 180);
+}
+
+export function countTons(saygData, accumulatorName) {
+    return count100(saygData, accumulatorName) + count140(saygData, accumulatorName) + (count180(saygData, accumulatorName) * 2);
+}
+
