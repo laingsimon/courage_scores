2dc73d85c96150a56740f96efe05018d572103d4
Simon Laing <laingsimon@users.noreply.github.com>
2023-06-24 17:31:03 +0100

    Update MatchReport.js

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchReport.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchReport.js
@@ -1,11 +1,5 @@
 import {useApp} from "../../../../AppContainer";
 
 export function MatchReport({ tournamentData, fixture }) {
-    const { onError } = useApp();
-
-    try {
-        return (<div className="page-break-after">MATCH REPORT</div>);
-    } catch (e) {
-        onError(e);
-    }
+    return (<div className="page-break-after">MATCH REPORT</div>);
 }
