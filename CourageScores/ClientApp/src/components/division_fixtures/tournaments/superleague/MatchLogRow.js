4c67ca33d7d4fe192ccb73c424ce27f0095abeb5
Simon Laing <laingsimon@users.noreply.github.com>
2023-06-24 17:01:52 +0100

    #327: show player name once

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchLogRow.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchLogRow.js
@@ -18,7 +18,7 @@ export function MatchLogRow({ leg, legNo, accumulatorName, player, noOfThrows, p
     }
 
     return (<tr className={winner ? 'bg-winner' : ''}>
-        <td>{player}</td>
+        {legNo === 1 ? (<td rowSpan={noOfLegs} className="align-middle bg-white">{player}</td>) : null}
         <td>{legNo}</td>
         <td>{sum(accumulator.throws, thr => thr.noOfDarts)}</td>
         <td>{winner ? lastThrow.score : 0}</td>
@@ -29,7 +29,7 @@ export function MatchLogRow({ leg, legNo, accumulatorName, player, noOfThrows, p
         <td>{countThrowsBetween(100, 140) + countThrowsBetween(140, 180) + (countThrowsBetween(180, 181) * 2)}</td>
         {playerOverallAverage === null || playerOverallAverage === undefined ? (<td>{round2dp(sumOverThrows('score') / sumOverThrows('noOfDarts'))}</td>) : null}
         {playerOverallAverage === null || playerOverallAverage === undefined || legNo > 1 ? null : (<td rowSpan={noOfLegs} className="align-middle bg-white fw-bold text-danger">{round2dp(playerOverallAverage)}</td>)}
-        <td className="text-danger">team average??</td>
+        {legNo === 1 ? (<td className="text-danger">team average??</td>) : null}
         <td>{lastThrow.noOfDarts}</td>
         {repeat(noOfThrows, i => {
             const playerThrow = accumulator.throws[i];
