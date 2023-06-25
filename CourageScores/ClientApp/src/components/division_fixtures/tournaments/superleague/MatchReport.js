a915333c354e963c6035f9d127b76e553b54e66f
Simon Laing <laingster@gmail.com>
2023-06-25 07:00:22 +0100

    #327: Show scores on match report

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchReport.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchReport.js
@@ -1,12 +1,25 @@
 import {useApp} from "../../../../AppContainer";
-import {getNoOfThrows} from "../../../../helpers/superleague";
+import {getNoOfLegs, getNoOfThrows} from "../../../../helpers/superleague";
 import {repeat} from "../../../../helpers/projection";
+import {MatchReportRow} from "./MatchReportRow";
 
 export function MatchReport({ tournamentData, fixture, saygDataMap, division }) {
     const { onError } = useApp();
     const round = tournamentData.round || {};
     const matches = round.matches || [];
     const noOfThrows = getNoOfThrows(matches, saygDataMap) + 1;
+    const noOfLegs = tournamentData.bestOf || getMaxNoOfLegs(saygDataMap);
+
+    function getMaxNoOfLegs(saygDataMap) {
+        let max = 0;
+
+        for (let id in saygDataMap) {
+            const saygData = saygDataMap[id];
+            max = Math.max(max, getNoOfLegs(saygData));
+        }
+
+        return max;
+    }
 
     try {
         return (<div className="page-break-after">
@@ -34,26 +47,31 @@ export function MatchReport({ tournamentData, fixture, saygDataMap, division }) 
                     <th>Ave</th>
                     <th>Players Name</th>
                     <th>Leg</th>
-                    {repeat(noOfThrows, i => <th>{i + 1}</th>)}
+                    {repeat(noOfThrows, i => <th key={`sideA_throwHeading_${i}`}>{i + 1}</th>)}
                     <th>AD</th>
                     <th>GS</th>
                     <th>SL</th>
                     <th>Tons</th>
                     <th>Ave</th>
                     <th>Players Name</th>
-                    <th>Leg</th>
-                    {repeat(noOfThrows, i => <th>{i + 1}</th>)}
+                    {repeat(noOfThrows, i => <th key={`sideB_throwHeading_${i}`}>{i + 1}</th>)}
                     <th>AD</th>
                     <th>GS</th>
                     <th>SL</th>
                     <th>Tons</th>
                 </tr>
                 </thead>
+                <tbody>
+                {matches.map((match, matchIndex) => {
+                    const saygData = saygDataMap[match.saygId];
+                    return (<MatchReportRow key={matchIndex} matchIndex={matchIndex} noOfLegs={noOfLegs} match={match} saygData={saygData} noOfThrows={noOfThrows} />);
+                })}
+                </tbody>
                 <tfoot>
                 <tr>
                     <td></td>
                     <td colSpan={7+noOfThrows} className="text-center">Legs won: ??</td>
-                    <td colSpan={7+noOfThrows} className="text-center">Legs won: ??</td>
+                    <td colSpan={6+noOfThrows} className="text-center">Legs won: ??</td>
                 </tr>
                 </tfoot>
             </table>
---
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchReportRow.js
@@ -0,0 +1,44 @@
+﻿import {useApp} from "../../../../AppContainer";
+import {repeat} from "../../../../helpers/projection";
+
+export function MatchReportRow({ match, matchIndex, saygData, noOfThrows, noOfLegs }) {
+    const { onError } = useApp();
+
+    try {
+        return (<>
+            {repeat(noOfLegs, legIndex => {
+                const leg = saygData.legs[legIndex + ''] || { home: {}, away: {} };
+
+                return (<tr key={`${match.id}_${legIndex}`}>
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>M{matchIndex + 1}</td>) : null}
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>ave</td>) : null}
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>{match.sideA.name}</td>) : null}
+                    <td>{legIndex + 1}</td>
+                    {repeat(noOfThrows, throwIndex => {
+                        const thr = (leg.home.throws ? leg.home.throws[throwIndex] : null) || {};
+                        const score = thr.bust ? 0 : thr.score;
+                        return (<td className={score >= 100 ? 'text-danger' : ''} key={`${match.id}_${legIndex}_sideA_${throwIndex}`}>{score}</td>);
+                    })}
+                    <td>AD</td>
+                    <td>GS</td>
+                    <td>SL</td>
+                    <td>Tons</td>
+
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>ave</td>) : null}
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>{match.sideB.name}</td>) : null}
+                    {repeat(noOfThrows, throwIndex => {
+                        const thr = (leg.away.throws ? leg.away.throws[throwIndex] : null) || {};
+                        const score = thr.bust ? 0 : thr.score;
+                        return (<td className={score >= 100 ? 'text-danger' : ''} key={`${match.id}_${legIndex}_sideB_${throwIndex}`}>{score}</td>);
+                    })}
+                    <td>AD</td>
+                    <td>GS</td>
+                    <td>SL</td>
+                    <td>Tons</td>
+                </tr>);
+            })}
+        </>);
+    } catch (e) {
+        onError(e);
+    }
+}
