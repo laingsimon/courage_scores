a4700fe577ed13c09567e48fb95fc45044c9c347
Simon Laing <laingster@gmail.com>
2023-06-25 07:13:45 +0100

    #327: Verically centre text on match report

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchReportRow.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchReportRow.js
@@ -55,9 +55,9 @@ export function MatchReportRow({ match, matchIndex, saygData, noOfThrows, noOfLe
                 }
 
                 return (<tr key={`${match.id}_${legIndex}`}>
-                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>M{matchIndex + 1}</td>) : null}
-                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>ave</td>) : null}
-                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>{match.sideA.name}</td>) : null}
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs} className="align-middle">M{matchIndex + 1}</td>) : null}
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs} className="align-middle fw-bold">ave</td>) : null}
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs} className="align-middle">{match.sideA.name}</td>) : null}
                     <td>{legIndex + 1}</td>
                     {repeat(noOfThrows, throwIndex => {
                         const thr = (leg.home.throws ? leg.home.throws[throwIndex] : null) || {};
@@ -69,8 +69,8 @@ export function MatchReportRow({ match, matchIndex, saygData, noOfThrows, noOfLe
                     <td>{scoreLeft('home')}</td>
                     <td>{countThrowsBetween('home', 100, 140) + countThrowsBetween('home', 140, 180) + (countThrowsBetween('home', 180) * 2)}</td>
 
-                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>ave</td>) : null}
-                    {legIndex === 0 ? (<td rowSpan={noOfLegs}>{match.sideB.name}</td>) : null}
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs} className="align-middle fw-bold">ave</td>) : null}
+                    {legIndex === 0 ? (<td rowSpan={noOfLegs} className="align-middle">{match.sideB.name}</td>) : null}
                     {repeat(noOfThrows, throwIndex => {
                         const thr = (leg.away.throws ? leg.away.throws[throwIndex] : null) || {};
                         const score = thr.bust ? 0 : thr.score;
