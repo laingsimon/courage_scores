4625c10e379eb24df4cfc10de05d020e1303d495
Simon Laing <laingster@gmail.com>
2023-06-25 06:34:21 +0100

    #327: Extract function into superleague helper

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchLog.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MatchLog.js
@@ -1,35 +1,13 @@
 import {MatchLogRow} from "./MatchLogRow";
 import {MatchLogTableHeading} from "./MatchLogTableHeading";
 import {useApp} from "../../../../AppContainer";
-import {getNoOfLegs, getPlayerOverallAverage} from "../../../../helpers/superleague";
+import {getNoOfLegs, getNoOfThrows, getPlayerOverallAverage} from "../../../../helpers/superleague";
 
 export function MatchLog({ tournamentData, fixture, saygDataMap }) {
     const { onError } = useApp();
     const round = tournamentData.round || {};
     const matches = round.matches || [];
-    const noOfThrows = getNoOfThrows() + 1;
-
-    function getNoOfThrows() {
-        let throws = 0;
-
-        for (let index = 0; index < matches.length; index++) {
-            const match = matches[index];
-            const saygData = saygDataMap[match.saygId];
-
-            if (!saygData) {
-                continue;
-            }
-
-            for (const legsKey in saygData.legs) {
-                const leg = saygData.legs[legsKey];
-
-                throws = Math.max(throws, leg.home.throws.length);
-                throws = Math.max(throws, leg.away.throws.length);
-            }
-        }
-
-        return throws;
-    }
+    const noOfThrows = getNoOfThrows(matches, saygDataMap) + 1;
 
     try {
         return (<div className="page-break-after">
--- CourageScores/ClientApp/src/helpers/superleague.js
+++ CourageScores/ClientApp/src/helpers/superleague.js
@@ -76,3 +76,25 @@ export function sumOfAllCheckouts(saygData, accumulatorName) {
                 : 0;
         }));
 }
+
+export function getNoOfThrows(matches, saygDataMap) {
+    let throws = 0;
+
+    for (let index = 0; index < matches.length; index++) {
+        const match = matches[index];
+        const saygData = saygDataMap[match.saygId];
+
+        if (!saygData) {
+            continue;
+        }
+
+        for (const legsKey in saygData.legs) {
+            const leg = saygData.legs[legsKey];
+
+            throws = Math.max(throws, leg.home.throws.length);
+            throws = Math.max(throws, leg.away.throws.length);
+        }
+    }
+
+    return throws;
+}
