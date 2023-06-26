cd19a3c3ee59e0e010cf1c8179a063f13ed1ebb2
Simon Laing <laingster@gmail.com>
2023-06-26 10:52:12 +0100

    Show tournament details on master draw

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MasterDraw.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/superleague/MasterDraw.js
@@ -1,5 +1,6 @@
 import {useApp} from "../../../../AppContainer";
 import {useTournament} from "../TournamentContainer";
+import {renderDate} from "../../../../helpers/rendering";
 
 export function MasterDraw() {
     const { tournamentData } = useTournament();
@@ -10,26 +11,35 @@ export function MasterDraw() {
     try {
         return (<div className="page-break-after">
             <h3>Master draw</h3>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>{tournamentData.host}</th>
-                    <th>v</th>
-                    <th>{tournamentData.opponent}</th>
-                </tr>
-                </thead>
-                <tbody>
-                {matches.map((m, index) => {
-                    return (<tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{m.sideA.name}</td>
-                        <td>v</td>
-                        <td>{m.sideB.name}</td>
-                    </tr>);
-                })}
-                </tbody>
-            </table>
+            <div className="d-flex flex-row">
+                <div>
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>{tournamentData.host}</th>
+                            <th>v</th>
+                            <th>{tournamentData.opponent}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {matches.map((m, index) => {
+                            return (<tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{m.sideA.name}</td>
+                                <td>v</td>
+                                <td>{m.sideB.name}</td>
+                            </tr>);
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="px-5">
+                    <div>Gender: <span className="fw-bold">{tournamentData.gender}</span></div>
+                    <div>Date: <span className="fw-bold">{renderDate(tournamentData.date)}</span></div>
+                    {tournamentData.notes ? (<div>Notes: <span className="fw-bold">{tournamentData.notes}</span></div>) : null}
+                </div>
+            </div>
         </div>);
     } catch (e) {
         onError(e);
