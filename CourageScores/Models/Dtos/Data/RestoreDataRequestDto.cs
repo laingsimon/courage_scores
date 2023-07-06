87bd7b9f8a0d8607c7e41dfdd200921933565c9d
Simon Laing <laingster@gmail.com>
2023-07-06 14:05:29 +0100

    #473: Express dto properties as nullable

--- CourageScores/Models/Dtos/Data/RestoreDataRequestDto.cs
+++ CourageScores/Models/Dtos/Data/RestoreDataRequestDto.cs
@@ -2,6 +2,6 @@ namespace CourageScores.Models.Dtos.Data;
 
 public class RestoreDataRequestDto : ImportDataRequestDto
 {
-    public string RequestToken { get; set; }
-    public string Identity { get; set; }
+    public string? RequestToken { get; set; }
+    public string? Identity { get; set; }
 }
