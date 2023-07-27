f274dc98827d021a69a64194ccb13dc1b2c4c0a8
Simon Laing <laingster@gmail.com>
2023-07-27 10:05:26 +0100

    #526: Exclude dto from code coverage

--- CourageScores/Models/Dtos/Health/HealthCheckResultDto.cs
+++ CourageScores/Models/Dtos/Health/HealthCheckResultDto.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace CourageScores.Models.Dtos.Health;
 
+[ExcludeFromCodeCoverage]
 public class HealthCheckResultDto
 {
     public List<string> Errors { get; set; } = new();
