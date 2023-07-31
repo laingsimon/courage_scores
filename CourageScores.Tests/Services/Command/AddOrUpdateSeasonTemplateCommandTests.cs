6cf928da4cdfce7c2ec374e48c5041d47c18ddc0
Simon Laing <laingster@gmail.com>
2023-07-31 09:48:24 +0100

    #527: Update season-template command to calculate template health on save

--- CourageScores.Tests/Services/Command/AddOrUpdateSeasonTemplateCommandTests.cs
+++ CourageScores.Tests/Services/Command/AddOrUpdateSeasonTemplateCommandTests.cs
@@ -1,7 +1,11 @@
+using CourageScores.Models.Adapters;
 using CourageScores.Models.Cosmos.Season.Creation;
+using CourageScores.Models.Dtos.Health;
 using CourageScores.Models.Dtos.Season.Creation;
 using CourageScores.Services.Command;
+using CourageScores.Services.Health;
 using CourageScores.Tests.Models.Adapters;
+using Moq;
 using NUnit.Framework;
 using CosmosSeason = CourageScores.Models.Cosmos.Season.Season;
 
@@ -25,7 +29,11 @@ public class AddOrUpdateSeasonTemplateCommandTests
     private readonly CancellationToken _token = new CancellationToken();
     private AddOrUpdateSeasonTemplateCommand _command = null!;
     private MockAdapter<Template, TemplateDto> _adapter = null!;
+    private Mock<IHealthCheckService> _healthCheckService = null!;
+    private Mock<ISimpleOnewayAdapter<Template,SeasonHealthDto>> _healthCheckAdapter = null!;
     private Template _template = null!;
+    private SeasonHealthDto _healthCheckDto = null!;
+    private SeasonHealthCheckResultDto _seasonHealthDto = null!;
 
     [SetUp]
     public void SetupEachTest()
@@ -36,12 +44,18 @@ public class AddOrUpdateSeasonTemplateCommandTests
             Name = "TEMPLATE",
             Updated = new DateTime(2001, 02, 03),
         };
+        _healthCheckDto = new SeasonHealthDto();
+        _seasonHealthDto = new SeasonHealthCheckResultDto();
         _adapter = new MockAdapter<Template, TemplateDto>();
-        _command = new AddOrUpdateSeasonTemplateCommand(_adapter);
+        _healthCheckService = new Mock<IHealthCheckService>();
+        _healthCheckAdapter = new Mock<ISimpleOnewayAdapter<Template, SeasonHealthDto>>();
+        _command = new AddOrUpdateSeasonTemplateCommand(_adapter, _healthCheckService.Object, _healthCheckAdapter.Object);
+        _healthCheckAdapter.Setup(a => a.Adapt(It.IsAny<Template>(), _token)).ReturnsAsync(_healthCheckDto);
+        _healthCheckService.Setup(s => s.Check(_healthCheckDto, _token)).ReturnsAsync(_seasonHealthDto);
     }
 
     [Test]
-    public async Task ApplyUpdate_WhenSeasonExists_UpdatesProperties()
+    public async Task ApplyUpdate_GivenTemplate_UpdatesProperties()
     {
         var update = new EditTemplateDto
         {
@@ -56,5 +70,22 @@ public class AddOrUpdateSeasonTemplateCommandTests
         Assert.That(_template.Name, Is.EqualTo(Template.Name));
         Assert.That(_template.Divisions, Is.EqualTo(Template.Divisions));
         Assert.That(_template.SharedAddresses, Is.EqualTo(Template.SharedAddresses));
+    }
+
+    [Test]
+    public async Task ApplyUpdate_GivenTemplate_RunsHealthCheck()
+    {
+        var update = new EditTemplateDto
+        {
+            LastUpdated = _template.Updated,
+        };
+        _adapter.AddMapping(Template, update);
+
+        var result = await _command.WithData(update).ApplyUpdate(_template, _token);
+
+        _healthCheckAdapter.Verify(s => s.Adapt(_template, _token));
+        _healthCheckService.Verify(s => s.Check(_healthCheckDto, _token));
+        Assert.That(result.Success, Is.True);
+        Assert.That(_template.TemplateHealth, Is.EqualTo(_seasonHealthDto));
     }
 }
--- CourageScores/Services/Command/AddOrUpdateSeasonTemplateCommand.cs
+++ CourageScores/Services/Command/AddOrUpdateSeasonTemplateCommand.cs
@@ -1,17 +1,26 @@
 ﻿using CourageScores.Models;
 using CourageScores.Models.Adapters;
 using CourageScores.Models.Cosmos.Season.Creation;
+using CourageScores.Models.Dtos.Health;
 using CourageScores.Models.Dtos.Season.Creation;
+using CourageScores.Services.Health;
 
 namespace CourageScores.Services.Command;
 
 public class AddOrUpdateSeasonTemplateCommand : AddOrUpdateCommand<Template, EditTemplateDto>
 {
     private readonly IAdapter<Template, TemplateDto> _templateAdapter;
+    private readonly IHealthCheckService _healthCheckService;
+    private readonly ISimpleOnewayAdapter<Template, SeasonHealthDto> _healthCheckAdapter;
 
-    public AddOrUpdateSeasonTemplateCommand(IAdapter<Template, TemplateDto> templateAdapter)
+    public AddOrUpdateSeasonTemplateCommand(
+        IAdapter<Template, TemplateDto> templateAdapter,
+        IHealthCheckService healthCheckService,
+        ISimpleOnewayAdapter<Template, SeasonHealthDto> healthCheckAdapter)
     {
         _templateAdapter = templateAdapter;
+        _healthCheckService = healthCheckService;
+        _healthCheckAdapter = healthCheckAdapter;
     }
 
     protected override async Task<ActionResult<Template>> ApplyUpdates(Template template, EditTemplateDto update, CancellationToken token)
@@ -21,7 +30,9 @@ public class AddOrUpdateSeasonTemplateCommand : AddOrUpdateCommand<Template, Edi
         template.Name = dto.Name;
         template.Divisions = dto.Divisions;
         template.SharedAddresses = dto.SharedAddresses;
-        template.TemplateHealth = null; // TODO: Set the template health
+
+        var result = await _healthCheckService.Check(await _healthCheckAdapter.Adapt(template, token), token);
+        template.TemplateHealth = result;
 
         return new ActionResult<Template>
         {
