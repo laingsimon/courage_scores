fa41da9532b2ded8f26b7dcf3e21e77b00ab70c4
Simon Laing <laingster@gmail.com>
2023-07-31 08:41:41 +0100

    #527: Add permission to control access to season template models

--- CourageScores/Models/Cosmos/Season/Creation/Template.cs
+++ CourageScores/Models/Cosmos/Season/Creation/Template.cs
@@ -1,13 +1,32 @@
 using System.Diagnostics.CodeAnalysis;
 using CourageScores.Models.Dtos.Health;
+using CourageScores.Models.Dtos.Identity;
 
 namespace CourageScores.Models.Cosmos.Season.Creation;
 
 [ExcludeFromCodeCoverage]
-public class Template : AuditedEntity
+public class Template : AuditedEntity, IPermissionedEntity
 {
     public string Name { get; set; } = null!;
     public List<DivisionTemplate> Divisions { get; set; } = new();
     public List<SharedAddress> SharedAddresses { get; set; } = new();
     public SeasonHealthCheckResultDto? TemplateHealth { get; set; }
+
+    [ExcludeFromCodeCoverage]
+    public bool CanCreate(UserDto? user)
+    {
+        return user?.Access?.ManageSeasonTemplates == true;
+    }
+
+    [ExcludeFromCodeCoverage]
+    public bool CanEdit(UserDto? user)
+    {
+        return user?.Access?.ManageSeasonTemplates == true;
+    }
+
+    [ExcludeFromCodeCoverage]
+    public bool CanDelete(UserDto? user)
+    {
+        return user?.Access?.ManageSeasonTemplates == true;
+    }
 }
