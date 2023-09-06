923cdd94a7feaf68fa31ff823602df700e1e8b81
Simon Laing <laingsimon@users.noreply.github.com>
2023-09-06 17:51:24 +0100

    #590:  add login prompt

--- CourageScores/ClientApp/src/components/admin/NotPermitted.js
+++ CourageScores/ClientApp/src/components/admin/NotPermitted.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 /* istanbul ignore next */
 export function NotPermitted() {
+    const {settings} = useDependencies();
+
+    function getAccountUrl(action) {
+        return `${settings.apiHost}/api/Account/${action}/?redirectUrl=${currentLink}`;
+    }
+
     return (<div className="content-background p-3 text-danger">
         <h3>⛔</h3>
         You're not permitted to use this function
+        <a className="btn btn-primary" href={`${getAccountUrl('Login')}`}>Login</a>
     </div>)
 }
