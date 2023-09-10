659890f316b0f7a0257bfca88c16cd372c403189
Simon Laing <laingsimon@users.noreply.github.com>
2023-09-10 13:16:00 +0100

    #600: update label links

--- CourageScores/ClientApp/src/components/division_fixtures/tournaments/Tournament.js
+++ CourageScores/ClientApp/src/components/division_fixtures/tournaments/Tournament.js
@@ -309,7 +309,7 @@ export function Tournament() {
                     : null}
                 {canManageTournaments
                     ? (<div className="form-group input-group mb-3 d-print-none">
-                        <label htmlFor="note-text" className="input-group-text">Options</label>
+                        <label className="input-group-text">Options</label>
                         <div className="form-control">
                             <div className="form-check form-switch margin-right my-1">
                                 <input disabled={saving} type="checkbox" className="form-check-input"
