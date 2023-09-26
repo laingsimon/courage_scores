2e5a5acfc960b791c7450683c95161feaf150aeb
Simon Laing <laingster@gmail.com>
2023-09-26 11:04:15 +0100

    #651: Introduce collapsed-text for dropdowns

--- CourageScores/ClientApp/src/components/division_fixtures/RefreshControl.js
+++ CourageScores/ClientApp/src/components/division_fixtures/RefreshControl.js
@@ -7,9 +7,9 @@ export function RefreshControl({ refreshInterval, setRefreshInterval, refresh })
 
     function getRefreshOptions() {
         return [
-            { value: 0, text: '⏸️ Paused' },
-            { value: 10000, text: '⏩ Live (Fast)' },
-            { value: 60000, text: '▶️ Live' },
+            { value: 0, text: '⏸️ Paused', collapsedText: '⏸️' },
+            { value: 10000, text: '⏩ Live (Fast)', collapsedText: '⏩' },
+            { value: 60000, text: '▶️ Live', collapsedText: '▶️' },
         ];
     }
 
@@ -42,7 +42,8 @@ export function RefreshControl({ refreshInterval, setRefreshInterval, refresh })
             className="margin-left float-end"
             options={getRefreshOptions()}
             onChange={setRefreshInterval}
-            value={refreshInterval} />
+            value={refreshInterval}
+            slim={true} />
         <span className="width-20 d-inline-block ms-2 text-secondary-50">{refreshing ? <LoadingSpinnerSmall /> : null}</span>
     </>);
 }
