c17ef15dded2753289e98d6b333656a6125b9fd2
Simon Laing <laingster@gmail.com>
2023-10-19 11:48:46 +0100

    #682: Exclude DTO from code coverage

--- CourageScores/Models/Dtos/Division/DataErrorDto.cs
+++ CourageScores/Models/Dtos/Division/DataErrorDto.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace CourageScores.Models.Dtos.Division;
 
+[ExcludeFromCodeCoverage]
 public class DataErrorDto
 {
     public string Message { get; set; } = null!;
