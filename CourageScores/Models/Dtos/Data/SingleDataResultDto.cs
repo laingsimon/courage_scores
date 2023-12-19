4bf5764e6926fc7e370f8985c642fb8f14dddd40
Simon Laing <laingster@gmail.com>
2023-12-19 14:18:36 +0000

    #705: Ignore null-valued properties from data browser

--- CourageScores/Models/Dtos/Data/SingleDataResultDto.cs
+++ CourageScores/Models/Dtos/Data/SingleDataResultDto.cs
@@ -1,9 +1,15 @@
+using Newtonsoft.Json;
+
 namespace CourageScores.Models.Dtos.Data;
 
 public class SingleDataResultDto : AuditedDto
 {
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public string? Name { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public DateTime? Date { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public Guid? DivisionId { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public Guid? SeasonId { get; set; }
 }
