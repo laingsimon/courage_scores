3695e74fc6bfe9cc2f56a36061e2fa5afdeb398e
Simon Laing <laingster@gmail.com>
2024-01-26 08:33:45 +0000

    #721: Replace CRA eslint packages with those required by vite

--- CourageScores/ClientApp/vite.config.ts
+++ CourageScores/ClientApp/vite.config.ts
@@ -2,12 +2,13 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import mkcert from 'vite-plugin-mkcert';
+import eslint from 'vite-plugin-eslint';
 
 /* istanbul ignore file */
 
 export default defineConfig({
     base: '',
-    plugins: [react(), viteTsconfigPaths(), mkcert()],
+    plugins: [react(), viteTsconfigPaths(), mkcert(), eslint()],
     server: {
         open: true,
         port: 44426,
