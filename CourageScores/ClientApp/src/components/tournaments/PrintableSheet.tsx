da8debc8cf3aac604969b7b5376c47fe631c4293
Simon Laing <laingster@gmail.com>
2024-02-07 07:34:36 +0000

    #743: Improve display of empty tournaments

--- CourageScores/ClientApp/src/components/tournaments/PrintableSheet.tsx
+++ CourageScores/ClientApp/src/components/tournaments/PrintableSheet.tsx
@@ -341,7 +341,7 @@ export function PrintableSheet({printOnly, editable}: IPrintableSheetProps) {
                 </span>
             </div>
             <div datatype="rounds-and-players"
-                 className="d-flex flex-row align-items-center overflow-auto no-overflow-on-print">
+                 className="d-flex flex-row align-items-center justify-content-center overflow-auto no-overflow-on-print">
                 {layoutData.map((roundData: ILayoutDataForRound, roundIndex: number) => (
                     <div key={roundIndex} datatype={`round-${roundIndex}`} className="d-flex flex-column p-3">
                         {roundIndex === layoutData.length - 1 ? render180s() : null}
@@ -366,6 +366,11 @@ export function PrintableSheet({printOnly, editable}: IPrintableSheetProps) {
                         </div>
                     </div>
                 </div>) : null}
+                {!any(tournamentData.sides) && editable
+                    ? (<div datatype="add-sides-hint" className="alert alert-warning m-3 d-print-none">
+                        Add who's playing by clicking <span className="text-secondary" onClick={() => setNewSide({ id: null })}>Add a side</span> &rarr;
+                    </div>)
+                    : null}
                 {any(tournamentData.sides) || editable ? (<div datatype="playing" className="ms-5">
                     <h4>Playing</h4>
                     <ul className="list-group">
