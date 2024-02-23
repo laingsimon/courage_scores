a35c6c0483572e0bfff6b48fd1ef9d70252f76a9
Simon Laing <laingster@gmail.com>
2024-02-23 13:28:27 +0000

    #773: Remove legacy web-socket implementation

--- CourageScores/ClientApp/src/live/IWebSocketContext.ts
+++ CourageScores/ClientApp/src/live/IWebSocketContext.ts
@@ -2,21 +2,6 @@
 
 export interface IWebSocketContext {
     webSocket?: WebSocket;
-    /*
-    * @deprecated
-    * TODO: Remove this when (legacy) LiveWebSocket is removed
-    * */
-    connectionAttempts?: number;
-    /*
-    * @deprecated
-    * TODO: Remove this when (legacy) LiveWebSocket is removed
-    * */
-    closures?: number;
-    /*
-    * @deprecated
-    * TODO: Remove this when (legacy) LiveWebSocket is removed
-    * */
-    banned?: boolean;
     pollingHandle?: number;
     modes: WebSocketMode[];
 }
