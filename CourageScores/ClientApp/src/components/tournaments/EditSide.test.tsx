611a8b3e8b5f33aaeb19d64f21194b201f618b16
Simon Laing <laingster@gmail.com>
2024-06-18 08:47:40 +0100

    #909: Determine divisionId for newly created player when multi-divisional

--- CourageScores/ClientApp/src/components/tournaments/EditSide.test.tsx
+++ CourageScores/ClientApp/src/components/tournaments/EditSide.test.tsx
@@ -69,6 +69,7 @@ describe('EditSide', () => {
                     address: '',
                     seasons: [{
                         seasonId: seasonId,
+                        divisionId: divisionId,
                         players: [playerDetailsAsTeamPlayer],
                     }]
                 }
