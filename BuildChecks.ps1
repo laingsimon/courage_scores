a75cfcc0ef94a74bb2f3a3e7d5ad94dbe450f1bc
Simon Laing <laingster@gmail.com>
2024-08-25 15:02:32 +0100

    #944: Add PR comment detailing files

--- BuildChecks.ps1
+++ BuildChecks.ps1
@@ -11,18 +11,58 @@ Function Get-Files($MinLines, $MaxLines)
         | Select-Object @{ label='row'; expression = {"| $($_.name) | $($_.lines) |" } }
 }
 
-Function Print-Files($Files) 
+Function Print-Files($Heading, $Files) 
 {
-    [Console]::Error.WriteLine("| File | Lines |")
-    [Console]::Error.WriteLine("| --- | --- |")
-    $Files | ForEach-Object { [Console]::Error.WriteLine($_.row) }
+    $Output=""
+    $Output += "| File | Lines |`n"
+    $Output += "| --- | --- |`n"
+
+    $Files | ForEach-Object { $Output += "$($_.row)`n" }
+
+    [Console]::Error.WriteLine($Heading)
+    [Console]::Error.WriteLine($Output)
+
+    Add-PullRequestComment -Markdown "### $($Heading)`n$($Output)"
+}
+
+Function Add-PullRequestComment($Markdown)
+{
+    If ($env:GITHUB_EVENT_NAME -ne "pull_request") 
+    {
+        [Console]::Error.WriteLine("Cannot add PR comment; workflow isn't running from a pull-request - $($env:GITHUB_EVENT_NAME)")
+        Return
+    }
+
+    $RefName=$env:GITHUB_REF_NAME # will be in the format <pr_number>/merge
+    $Token=$env:GITHUB_TOKEN
+    $PullRequestNumber=$RefName.Replace("/merge", "")
+    $Body = "{""body"": ""$($Markdown.Replace("`n", "\n"))""}"
+    $Repo = $env:GITHUB_REPOSITORY
+    $Url="https://api.github.com/repos/$($Repo)/issues/$($PullRequestNumber)/comments"
+
+    # [Console]::Out.WriteLine("Sending POST request to $($Url) with body $($Body)")
+
+    $Response = Invoke-WebRequest `
+        -Uri $Url `
+        -Headers @{
+            Accept="application/vnd.github+json";
+            Authorization="Bearer $($Token)";
+            # X-GitHub-Api-Version="2022-11-28";
+        } `
+        -Method Post `
+        -Body $Body
+
+    if ($Response.StatusCode -ne 201) 
+    {
+        $Response
+        Write-Error "Error creating comment"
+    }
 }
 
 $FilesOverThreshold = Get-Files -MinLines $ErrorThreshold -MaxLines [int]::MaxValue
 If ($ErrorThreshold -gt 0 -and $FilesOverThreshold.Length -gt 0)
 {
-    [Console]::Error.WriteLine("Files exceeding limit")
-    Print-Files -Files $FilesOverThreshold
+    Print-Files -Heading "Files exceeding limit" -Files $FilesOverThreshold
     [Console]::Error.WriteLine("There are $($FilesOverThreshold.Length) file/s that have more than $($ErrorThreshold) lines")
 }
 
@@ -37,8 +77,7 @@ else
 
 If ($WarningThreshold -gt 0 -and $FilesNearingLimit.Length -gt 0)
 {
-    [Console]::Error.WriteLine("Files approaching limit")
-    Print-Files -Files $FilesNearingLimit
+    Print-Files -Heading "Files approaching limit" -Files $FilesNearingLimit
 }
 
 Exit $FilesOverThreshold.Length
