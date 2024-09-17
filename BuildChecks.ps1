570b94fe9f52151c164127b2bfe7703819332226
Simon Laing <laingster@gmail.com>
2024-09-17 08:34:52 +0100

    Fix replacement of line-limit comments

--- BuildChecks.ps1
+++ BuildChecks.ps1
@@ -53,7 +53,7 @@ Function Get-PullRequestComments()
 
     $Json = $Response | ConvertFrom-Json
     Return $Json `
-        | Where-Object { $_.body -like "*$($Extension) file/s approaching limit*" -Or $_.body.Value -like "*$($Extension) file/s exceeding limit*" } `
+        | Where-Object { $_.body -like "*$($Extension) file/s approaching line*" -Or $_.body.Value -like "*$($Extension) file/s exceeding*" } `
 }
 
 Function Remove-ExistingComment($Comment)
