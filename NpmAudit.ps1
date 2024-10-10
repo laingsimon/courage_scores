06e1ba475ae0c9322777d6ebb8e3951692b2fbb3
Simon Laing <laingster@gmail.com>
2024-10-10 07:54:12 +0100

    #1055: Run npm-audit via script

---
+++ NpmAudit.ps1
@@ -0,0 +1,148 @@
+param([string] $NpmAuditBypassCveWarnings)
+$CommentHeading = "npm audit report"
+
+
+Function Get-PullRequestComments() 
+{
+    If ($env:GITHUB_EVENT_NAME -ne "pull_request") 
+    {
+        [Console]::Error.WriteLine("Cannot add PR comment; workflow isn't running from a pull-request - $($env:GITHUB_EVENT_NAME)")
+        $EmptyList = @()
+        Return ,$EmptyList
+    }
+
+    $Url="https://api.github.com/repos/$($Repo)/issues/$($PullRequestNumber)/comments"
+    Write-Message "Get pull-request comments"
+
+    $Response = Invoke-WebRequest `
+        -Uri $Url `
+        -Headers @{
+            Authorization="Bearer $($Token)";
+        } `
+        -Method Get `
+
+    if ($Response.StatusCode -ne 200) 
+    {
+        $Response
+        Write-Error "Error getting comment"
+    }
+
+    $Json = $Response | ConvertFrom-Json
+    Return $Json `
+        | Where-Object { $_.body -like "*$($CommentHeading)*" } `
+}
+
+Function Remove-ExistingComment($Comment)
+{
+    $CommentId = $Comment.id
+    $Url = $Comment.url
+    Write-Message "Deleting comment '$($CommentId)'"
+
+    $Response = Invoke-WebRequest `
+        -Uri $Url `
+        -Headers @{
+            Accept="application/vnd.github+json";
+            Authorization="Bearer $($Token)";
+        } `
+        -Method Delete
+
+    if ($Response.StatusCode -ne 204) 
+    {
+        Write-Error "Error deleting comment at url $($Url)"
+        $Response
+    }
+}
+
+Function Remove-ExistingComments() 
+{
+    If ($env:GITHUB_EVENT_NAME -ne "pull_request") 
+    {
+        [Console]::Error.WriteLine("Cannot remove existing PR comments; workflow isn't running from a pull-request - $($env:GITHUB_EVENT_NAME)")
+        Return
+    }
+
+    Write-Message "Remove existing comments: $($Comments.Count)"
+    $Comments | ForEach-Object { Remove-ExistingComment -Comment $_ }
+}
+
+Function Write-Message($Message)
+{
+    [Console]::Out.WriteLine($Message)
+}
+
+Function Add-PullRequestComment($Markdown)
+{
+    If ($env:GITHUB_EVENT_NAME -ne "pull_request") 
+    {
+        [Console]::Error.WriteLine("Cannot add PR comment; workflow isn't running from a pull-request - $($env:GITHUB_EVENT_NAME)")
+        Return
+    }
+
+    $Body = "{""body"": ""$($Markdown.Replace("`n", "\n"))""}"
+    $Url="https://api.github.com/repos/$($Repo)/issues/$($PullRequestNumber)/comments"
+
+    Write-Message "Sending POST request to $($Url) with body $($Body)"
+
+    $Response = Invoke-WebRequest `
+        -Uri $Url `
+        -Headers @{
+            Accept="application/vnd.github+json";
+            Authorization="Bearer $($Token)";
+        } `
+        -Method Post `
+        -Body $Body
+
+    if ($Response.StatusCode -ne 201) 
+    {
+        Write-Error "Error creating comment at url $($Url)"
+        $Response
+    }
+}
+
+Function Run-NpmAudit()
+{
+    $processStartInfo = [System.Diagnostics.ProcessStartInfo]::new()
+    $processStartInfo.FileName = 'npm'
+    $processStartInfo.Arguments = 'audit'
+    $processStartInfo.RedirectStandardOutput = $true
+    $processStartInfo.RedirectStandardError = $true
+    $processStartInfo.UseShellExecute = $true
+    $process = [System.Diagnostics.Process]::new()
+    $process.StartInfo = $processStartInfo
+    $success = $process.Start()
+    $stdOut = $process.StandardOutput.ReadToEnd()
+    $stdErr = $process.StandardError.ReadToEnd()
+    $exitCode = $process.ExitCode
+
+    return @{
+        success = $success;
+        output = $stdOut;
+        error = $stdErr;
+        exitCode = $exitCode;
+    }
+}
+
+$RefName=$env:GITHUB_REF_NAME # will be in the format <pr_number>/merge
+$Token=$env:GITHUB_TOKEN
+If ($RefName -ne $null)
+{
+    $PullRequestNumber=$RefName.Replace("/merge", "")
+}
+else
+{
+    $PullRequestNumber = ""
+}
+$Repo = $env:GITHUB_REPOSITORY
+$Comments = Get-PullRequestComments
+Remove-ExistingComments
+
+$NpmAuditResult = Run-NpmAudit
+Write-Output $NpmAuditResult.output
+Write-Error $NpmAuditResult.error
+If ($NpmAuditResult.ExitCode -ne 0)
+{
+    # add a comment to the PR if content isn't empty - must contain $CommentHeading
+    Add-PullRequestComment "#### $($CommentHeading)`n`n$($NpmAuditResult.output)`n`n$($NpmAuditResult.error)"
+}
+
+Exit $NpmAuditResult.exitCode
