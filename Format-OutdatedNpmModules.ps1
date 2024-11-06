2ca6b4c6996a1ba9195bcb7d465ce3d81ff009ab
Simon Laing <laingsimon@users.noreply.github.com>
2024-11-06 15:13:41 +0000

    copy methods to poweshell module

---
+++ Format-OutdatedNpmModules.ps1
@@ -0,0 +1,20 @@
+param([string] $Prefix, [string] $FileName)
+
+Import-Module -Name "$PSScriptRoot/NpmFunctions.psm1"
+
+$NpmOutdatedResult = Invoke-NpmCommand -Command "outdated --parseable"
+
+If ($NpmOutdatedResult.ExitCode -ne 0)
+{
+    $Output = "#### $($OutdatedCommentHeading)`n$(Format-NpmOutdatedContent -output $NpmOutdatedResult.output -error $NpmOutdatedResult.error)"
+}
+else
+{
+    $Output = "no outdated modules"
+}
+
+Write-Output $Output
+If ($FileName -ne "")
+{
+    "$($Prefix)$($Output)" | Out-File -Path $FileName
+}
