ccb0e8f38bc409382d535d635c3c0bef572ba394
Simon Laing <laingster@gmail.com>
2025-02-02 14:55:47 +0000

    #1198: Display an error if the sayg data is not found

--- CourageScores/ClientApp/src/components/sayg/LiveSayg.tsx
+++ CourageScores/ClientApp/src/components/sayg/LiveSayg.tsx
@@ -2,6 +2,8 @@ import {useParams} from "react-router";
 import {SaygLoadingContainer} from "./SaygLoadingContainer";
 import {ILiveOptions} from "../../live/ILiveOptions";
 import {LiveDataType} from "../../interfaces/models/dtos/Live/LiveDataType";
+import {useState} from "react";
+import {asyncCallback} from "../../helpers/events";
 
 export function LiveSayg() {
     const { id } = useParams();
@@ -10,14 +12,20 @@ export function LiveSayg() {
         canSubscribe: true,
         subscribeAtStartup: [{ id: id!, type: LiveDataType.sayg }],
     };
+    const [loadError, setLoadError] = useState<string | null>(null);
 
     return (<div className="content-background p-3 pb-1">
+        {loadError ? <div className="mb-3">
+            <h3>Error loading match data</h3>
+            <p className="text-danger">{loadError}</p>
+        </div> : null}
         <SaygLoadingContainer
             id={id!}
             matchStatisticsOnly={true}
             autoSave={false}
             liveOptions={liveOptions}
             lastLegDisplayOptions={ { showThrows: true, showAverage: true } }
+            onLoadError={asyncCallback(setLoadError)}
             />
     </div>);
 }
