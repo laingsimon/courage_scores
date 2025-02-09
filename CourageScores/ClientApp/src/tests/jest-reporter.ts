2caab688938c4b696195d7bfe0cc5b438812e56e
Simon Laing <laingster@gmail.com>
2025-02-09 10:42:31 +0000

    Add skeleton Jest reporter

---
+++ CourageScores/ClientApp/src/tests/jest-reporter.ts
@@ -0,0 +1,31 @@
+﻿const fs = require('fs');
+
+class JestReporter {
+    constructor(globalConfig, reporterOptions, reporterContext) {
+    }
+
+    onRunComplete(testContexts, results) {
+        if (results.success) {
+            return;
+        }
+
+        const testResults = results.testResults; // []
+        for (const testResult of testResults ) {
+            const childResults = testResult.testResults; // []
+            for (const childResult of childResults ) {
+                const title = childResult.ancestorTitles.join(' > ');
+                const status = childResult.status;
+
+                if (status === 'passed') {
+                    continue;
+                }
+
+                console.log(`${title}: ${status}`);
+            }
+        }
+
+        // console.log(`onRunComplete arguments: ${JSON.stringify(arguments)}`)
+    }
+}
+
+module.exports = JestReporter;
