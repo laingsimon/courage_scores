0bd55c9520e45bd860700970f7f8d04770dc077f
Simon Laing <laingster@gmail.com>
2025-02-25 19:40:03 +0000

    Fix script when running out of a PR context

--- Scripts/ProcessTestFailures.ps1
+++ Scripts/ProcessTestFailures.ps1
@@ -168,25 +168,25 @@ $TestsCommentHeading = "Failed tests"
 
 Write-Message "GITHUB_JOB=$($GitHubJob), GITHUB_RUN_ATTEMPT=$($GitHubRunAttempt), GITHUB_RUN_ID=$($GitHubRunId), GITHUB_RUN_NUMBER=$($GitHubRunNumber)"
 
-if ($PullRequestNumber -eq "main" -and $GitHubEvent -eq "push")
-{
-    # find the pull request for main
-    $PullRequest = Get-PullRequests -Base "main"
-    if ($PullRequest -ne $null)
-    {
-        $PullRequestNumber = "$($PullRequest.number)"
-        $GitHubEvent = "pull_request"
-    }
-}
-
-if ($GitHubEvent -ne "pull_request" -or $PullRequestNumber -eq "")
-{
-    Write-Message "Not triggered (or able to find the relevant) pull request"
-    Exit
-}
-
 try
 {
+    if ($PullRequestNumber -eq "main" -and $GitHubEvent -eq "push")
+    {
+        # find the pull request for main
+        $PullRequest = Get-PullRequests -Base "main"
+        if ($PullRequest -ne $null)
+        {
+            $PullRequestNumber = "$($PullRequest.number)"
+            $GitHubEvent = "pull_request"
+        }
+    }
+
+    if ($GitHubEvent -ne "pull_request" -or $PullRequestNumber -eq "")
+    {
+        Write-Message "Not triggered (or able to find the relevant) pull request"
+        Exit
+    }
+
     $Comments = [array] (Get-PullRequestComments $TestsCommentHeading)
     Remove-ExistingComments -Comments $Comments
 
