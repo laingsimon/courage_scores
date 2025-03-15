13ef3d137aa850eddad814558f208a019d1230ad
Simon Laing <laingsimon@users.noreply.github.com>
2025-03-15 14:59:26 +0000

    #1369: enter full screen when sayg open

--- CourageScores/ClientApp/src/components/scores/MatchPlayerSelection.tsx
+++ CourageScores/ClientApp/src/components/scores/MatchPlayerSelection.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {ISelectablePlayer, PlayerSelection} from "../common/PlayerSelection";
 import {Dialog} from "../common/Dialog";
 import {any, distinct} from "../../helpers/collections";
@@ -32,12 +32,20 @@ export interface IMatchPlayerSelectionProps {
 }
 
 export function MatchPlayerSelection({match, onMatchChanged, onMatchOptionsChanged, on180, onHiCheck}: IMatchPlayerSelectionProps) {
-    const {account, onError} = useApp();
+    const {account, onError, fullScreen} = useApp();
     const {homePlayers, awayPlayers, readOnly, disabled, division, season, home, away} = useLeagueFixture();
     const {matchOptions, otherMatches, setCreatePlayerFor} = useMatchType();
     const [matchOptionsDialogOpen, setMatchOptionsDialogOpen] = useState<boolean>(false);
     const [saygOpen, setSaygOpen] = useState<boolean>(false);
     const hasBothScores = hasScore(match.homeScore) && hasScore(match.awayScore);
+
+    useEffect(() => {
+        if (saygOpen) {
+            fullScreen.enterFullScreen();
+        } else {
+            fullScreen.exitFullScreen();
+        }
+    }, [saygOpen]);
 
     function player(index: number, side: 'home' | 'away'): GamePlayerDto {
         const matchPlayers: GamePlayerDto[] = match[side + 'Players'];
