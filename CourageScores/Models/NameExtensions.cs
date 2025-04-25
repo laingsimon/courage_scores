a6ab5ea2ddff765ad329fd5f78acb63fadb6f841
Simon Laing <laingster@gmail.com>
2025-04-25 12:29:17 +0100

    Tweaks to reduce over build warnings

--- CourageScores/Models/NameExtensions.cs
+++ CourageScores/Models/NameExtensions.cs
@@ -4,8 +4,19 @@ public static class NameExtensions
 {
     public static string AddressOrName(this INameAndAddress item, string fallbackValue = "")
     {
-        return (string.IsNullOrEmpty(item.Address?.Trim())
-            ? item.Name?.Trim()
-            : item.Address?.Trim()) ?? fallbackValue;
+        var address = TrimPossiblyNullString(item.Address);
+        var name = TrimPossiblyNullString(item.Name);
+
+        return (string.IsNullOrEmpty(address)
+            ? name
+            : address) ?? fallbackValue;
+    }
+
+    private static string? TrimPossiblyNullString(string? value)
+    {
+        // within the dotnet code we can be fairly confident that `value` will never be null, however if a null value
+        // is included in some json content then it could result in a never-null property being set to null, as such
+        // we check for null before trying to trim the value
+        return value?.Trim();
     }
 }
