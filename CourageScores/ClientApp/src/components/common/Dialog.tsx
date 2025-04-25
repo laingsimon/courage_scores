f96634faa9c6e1dbfb612dc7d13f573613dbc25a
Simon Laing <laingster@gmail.com>
2025-04-25 11:19:31 +0100

    #1474: Centre align (vertically) checkout darts dialog

--- CourageScores/ClientApp/src/components/common/Dialog.tsx
+++ CourageScores/ClientApp/src/components/common/Dialog.tsx
@@ -14,7 +14,7 @@ export interface IDialogProps {
 export function Dialog({children, onClose, title, slim, className, contentWidth, fullScreen}: IDialogProps) {
     return (<div className="text-start">
         <div className={`modal fade show text-black ${className || ''}`} role="dialog" style={{display: 'block'}}>
-            <div className={`modal-dialog${fullScreen ? ' position-static' : ' modal-dialog-centered'}${slim || fullScreen ? '' : 'modal-dialog-larger-max-width'}${contentWidth ? ' modal-sm' : ''}`}>
+            <div className={`modal-dialog${fullScreen ? ' position-static' : ' modal-dialog-centered'}${slim || fullScreen ? '' : ' modal-dialog-larger-max-width'}${contentWidth ? ' modal-sm' : ''}`}>
                 <div className={`modal-content${fullScreen ? ' position-absolute top-0 bottom-0 left-0 right-0 border-0' : ''}`}>
                     {title ? (<div className="modal-header justify-content-center">
                         <h5>{title}</h5>
