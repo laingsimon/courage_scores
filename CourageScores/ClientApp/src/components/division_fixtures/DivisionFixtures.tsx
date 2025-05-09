23a32573baef79a8c23b777ad94a9fe4878f5d8e
Simon Laing <laingster@gmail.com>
2025-05-09 07:58:32 +0100

    #1512: Ignore lines from coverage

--- CourageScores/ClientApp/src/components/division_fixtures/DivisionFixtures.tsx
+++ CourageScores/ClientApp/src/components/division_fixtures/DivisionFixtures.tsx
@@ -209,7 +209,9 @@ export function DivisionFixtures({setNewFixtures}: IDivisionFixturesProps) {
     }
 
     async function bulkDeleteFixtures() {
+        /* istanbul ignore next */
         if (!season || deletingAllFixtures) {
+            /* istanbul ignore next */
             return;
         }
 
