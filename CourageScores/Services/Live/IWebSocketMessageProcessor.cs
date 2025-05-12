f65cd4d0ab909711023d06f13667fbfc4ded06ea
Simon Laing <laingster@gmail.com>
2025-05-12 20:54:05 +0100

    #1540: Patching a tournament should publish updates to subscribers

--- CourageScores/Services/Live/IWebSocketMessageProcessor.cs
+++ CourageScores/Services/Live/IWebSocketMessageProcessor.cs
@@ -6,8 +6,6 @@ namespace CourageScores.Services.Live;
 public interface IWebSocketMessageProcessor
 {
     void Disconnected(IWebSocketContract socket);
-
-    Task PublishUpdate(IWebSocketContract source, Guid id, LiveDataType dataType, object dto, CancellationToken token);
-
+    Task PublishUpdate(IWebSocketContract? source, Guid id, LiveDataType dataType, object dto, CancellationToken token);
     IAsyncEnumerable<WatchableData> GetWatchableData(CancellationToken token);
 }
