bc2e030354da054564a7a152d78b5fa81ad56209
Simon Laing <laingsimon@users.noreply.github.com>
2025-07-23 07:20:02 +0100

    Correct method name

--- Scripts/CreateRelease.ps1
+++ Scripts/CreateRelease.ps1
@@ -152,7 +152,7 @@ function Format-ReleaseDescription($Commits, $Milestone)
         }
         else
         {
-            $Ancillary.Add(  ( Format-AncillaryMessage -Message $Commit.comment.Trim() )  )
+            $Ancillary.Add(  ( Format-AncillaryChange -Message $Commit.comment.Trim() )  )
         }
     }
     
