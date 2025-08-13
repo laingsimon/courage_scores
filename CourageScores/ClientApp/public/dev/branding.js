9e2fe054d88d329abb1a9631d7fd975eb647f06f
Simon Laing <simon.laing@gearset.com>
2025-08-13 21:13:59 +0100

    #1672: Move branding to importable file to improve security posture via CSP

---
+++ CourageScores/ClientApp/public/dev/branding.js
@@ -0,0 +1,26 @@
+window.branding = {
+    name: 'Courage Scores',
+    email: 'info@couragescores.co.uk',
+    facebook: 'unset',
+    twitter: 'unset',
+    website: 'https://github.com/laingsimon/courage_scores',
+    custodians: [
+        'Simon Laing'
+    ],
+    menu: {
+        beforeDivisions: [{
+            text: 'Project page',
+            url: 'https://github.com/laingsimon/courage_scores',
+        }, {
+            text: 'Practice',
+            url: '/practice',
+        }],
+        afterDivisions: [{
+            text: 'Help',
+            url: 'https://github.com/laingsimon/courage_scores/wiki',
+        }, {
+            text: 'About',
+            url: '/about',
+        }]
+    }
+};
