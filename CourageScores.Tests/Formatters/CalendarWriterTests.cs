0f58f652e86dd9ee6c2f8a5d2c738c380c2b453f
Simon Laing <laingster@gmail.com>
2025-08-30 16:49:28 +0100

    #1697: Add calendar writer

---
+++ CourageScores.Tests/Formatters/CalendarWriterTests.cs
@@ -0,0 +1,441 @@
+﻿using CourageScores.Formatters;
+using NUnit.Framework;
+
+namespace CourageScores.Tests.Formatters;
+
+[TestFixture]
+public class CalendarWriterTests
+{
+    private CalendarWriter _writer = null!;
+    private StringWriter _textWriter = null!;
+    private CancellationToken _token;
+
+    [SetUp]
+    public void SetupEachTest()
+    {
+        _writer = new CalendarWriter();
+        _textWriter = new StringWriter();
+        _token = CancellationToken.None;
+    }
+
+    [Test]
+    public async Task WriteToStream_WhenCalled_WritesBeginAndEndTags()
+    {
+        var calendar = new Calendar();
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        var content = _textWriter.GetStringBuilder().ToString();
+        Assert.That(content, Does.StartWith("BEGIN:VCALENDAR" + Environment.NewLine));
+        Assert.That(content, Does.EndWith("END:VCALENDAR" + Environment.NewLine));
+    }
+
+    [TestCase("", "PRODID:")]
+    [TestCase("id", "PRODID:id")]
+    public async Task WriteToStream_WhenCalled_WritesProdId(string id, string expected)
+    {
+        var calendar = new Calendar
+        {
+            Id = id,
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain(expected + Environment.NewLine));
+    }
+
+    [TestCase("", "METHOD:")]
+    [TestCase("method", "METHOD:method")]
+    public async Task WriteToStream_WhenCalled_WritesMethod(string method, string expected)
+    {
+        var calendar = new Calendar
+        {
+            Method = method,
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain(expected + Environment.NewLine));
+    }
+
+    [TestCase(null, "NAME:", false)]
+    [TestCase("", "NAME:", false)]
+    [TestCase("name", "NAME:name", true)]
+    public async Task WriteToStream_WhenCalled_WritesName(string? name, string expected, bool contains)
+    {
+        var calendar = new Calendar
+        {
+            Name = name,
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            contains
+                ? Does.Contain(expected + Environment.NewLine)
+                : Does.Not.Contain(expected));
+    }
+
+    [TestCase(null, "DESCRIPTION:", false)]
+    [TestCase("", "DESCRIPTION:", false)]
+    [TestCase("description", "DESCRIPTION:description", true)]
+    public async Task WriteToStream_WhenCalled_WritesDescription(string? description, string expected, bool contains)
+    {
+        var calendar = new Calendar
+        {
+            Description = description,
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            contains
+                ? Does.Contain(expected + Environment.NewLine)
+                : Does.Not.Contain(expected));
+    }
+
+    [Test]
+    public async Task WriteToStream_WhenCalled_WritesTimeZoneId()
+    {
+        var calendar = new Calendar();
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain($"TIMEZONE-ID:{TimeZoneInfo.Local.StandardName}" + Environment.NewLine));
+    }
+
+    [TestCase(null, "REFRESH-INTERVAL;VALUE=DURATION:", false)]
+    [TestCase("0.12:00:00.000", "REFRESH-INTERVAL;VALUE=DURATION:P12H", true)]
+    public async Task WriteToStream_WhenCalled_WritesRefreshInterval(string? interval, string expected, bool contains)
+    {
+        var calendar = new Calendar
+        {
+            RefreshInterval = interval != null
+                ? TimeSpan.Parse(interval)
+                : null,
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            contains
+                ? Does.Contain(expected + Environment.NewLine)
+                : Does.Not.Contain(expected));
+    }
+
+    [Test]
+    public async Task WriteToStream_GivenEvent_WritesBeginAndEndEventTags()
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        var content = _textWriter.GetStringBuilder().ToString();
+        Assert.That(content, Does.Contain("BEGIN:VEVENT" + Environment.NewLine));
+        Assert.That(content, Does.Contain("END:VEVENT" + Environment.NewLine));
+    }
+
+    [Test]
+    public async Task WriteToStream_GivenEvent_WritesSummary()
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain("SUMMARY:title" + Environment.NewLine));
+    }
+
+    [Test]
+    public async Task WriteToStream_GivenEvent_WritesUid()
+    {
+        var id = Guid.NewGuid();
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    Id = id,
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain("UID:" + id + Environment.NewLine));
+    }
+
+    [Test]
+    public async Task WriteToStream_GivenEvent_WritesSequence()
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    Version = 2,
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain("SEQUENCE:2" + Environment.NewLine));
+    }
+
+    [TestCase(false, "STATUS:TENTATIVE")]
+    [TestCase(true, "STATUS:CONFIRMED")]
+    public async Task WriteToStream_GivenEvent_WritesStatus(bool confirmed, string expected)
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    Confirmed = confirmed,
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain(expected + Environment.NewLine));
+    }
+
+    [TestCase(false, "TRANSP:TRANSPARENT")]
+    [TestCase(true, "TRANSP:OPAQUE")]
+    public async Task WriteToStream_GivenEvent_WritesPrivacy(bool @private, string expected)
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    Private = @private,
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain(expected + Environment.NewLine));
+    }
+
+    [Test]
+    public async Task WriteToStream_GivenEvent_WritesStartAndEndTime()
+    {
+        var startTime = new DateTime(2001, 02, 03, 04, 05, 06);
+        var endTime = new DateTime(2002, 03, 04, 05, 06, 07);
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    FromInclusive = startTime,
+                    ToExclusive = endTime,
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        var content = _textWriter.GetStringBuilder().ToString();
+        Assert.That(
+            content,
+            Does.Contain("DTSTART:20010203T040506" + Environment.NewLine));
+        Assert.That(
+            content,
+            Does.Contain("DTEND:20020304T050607" + Environment.NewLine));
+    }
+
+    [Test]
+    public async Task WriteToStream_GivenEvent_WritesTimestamp()
+    {
+        var lastUpdated = new DateTime(2001, 02, 03, 04, 05, 06);
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    LastUpdated = lastUpdated,
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain("DTSTAMP:20010203T040506" + Environment.NewLine));
+    }
+
+    [TestCase("", "CATEGORIES:", false)]
+    [TestCase("a", "CATEGORIES:a", true)]
+    [TestCase("a,b", "CATEGORIES:a,b", true)]
+    public async Task WriteToStream_GivenEvent_WritesCategories(string categories, string expected, bool contains)
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    Categories = string.IsNullOrEmpty(categories)
+                        ? []
+                        : categories.Split(',').ToList(),
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            contains
+                ? Does.Contain(expected + Environment.NewLine)
+                : Does.Not.Contain(expected));
+    }
+
+    [TestCase(null, "URL:", false)]
+    [TestCase("", "URL:", false)]
+    [TestCase("https://localhost/", "URL:https://localhost/", true)]
+    public async Task WriteToStream_GivenEvent_WritesUrl(string url, string expected, bool contains)
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    Url = string.IsNullOrEmpty(url)
+                        ? null
+                        : new Uri(url),
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            contains
+                ? Does.Contain(expected + Environment.NewLine)
+                : Does.Not.Contain(expected));
+    }
+
+    [TestCase(null, "LOCATION:", false)]
+    [TestCase("", "LOCATION:", false)]
+    [TestCase("location", "LOCATION:location", true)]
+    public async Task WriteToStream_GivenEvent_WritesLocation(string location, string expected, bool contains)
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = "title",
+                    Location = location,
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            contains
+                ? Does.Contain(expected + Environment.NewLine)
+                : Does.Not.Contain(expected));
+    }
+
+    //   ----------------------------------------------------------------------
+    //                                                         70th character ^
+    [TestCase(
+        "short_input",
+        "SUMMARY:short_input")]
+    [TestCase(
+        "short_input\nwith return lines",
+        "SUMMARY:short_input\\nwith return lines")]
+    [TestCase(
+        "short_input\r\nwith CRLF return lines",
+        "SUMMARY:short_input\\nwith CRLF return lines")]
+    [TestCase(
+        "input that is exactly 70 characters looooooooooooooooooooooooooooooong",
+        "SUMMARY:input that is exactly 70 characters looooooooooooooooooooooooooooooong")]
+    [TestCase(
+        "input that is exactly  71 characters long and ends with a return line\n",
+        "SUMMARY:input that is exactly  71 characters long and ends with a return line")]
+    [TestCase(
+        "input that that contains a return line character at character num  70\\n then more content",
+        "SUMMARY:input that that contains a return line character at character num  70\\n\n  then more content")]
+    public async Task WriteToStream_GivenStrings_EncodesStringAccordingToSpecification(string input, string expected)
+    {
+        var calendar = new Calendar
+        {
+            Events =
+            {
+                new CalendarEvent
+                {
+                    Title = input,
+                }
+            }
+        };
+
+        await _writer.WriteToStream(calendar, _textWriter, _token);
+
+        Assert.That(
+            _textWriter.GetStringBuilder().ToString(),
+            Does.Contain(expected + Environment.NewLine));
+    }
+}
---
+++ CourageScores/Formatters/CalendarWriter.cs
@@ -0,0 +1,110 @@
+﻿namespace CourageScores.Formatters;
+
+public class CalendarWriter : ICalendarWriter
+{
+    public async Task WriteToStream(Calendar calendar, TextWriter textWriter, CancellationToken token)
+    {
+        await textWriter.WriteLineAsync("BEGIN:VCALENDAR");
+        await textWriter.WriteLineAsync("VERSION:2.0");
+        await textWriter.WriteLineAsync($"PRODID:{calendar.Id}");
+        await textWriter.WriteLineAsync("CALSCALE:GREGORIAN");
+        await textWriter.WriteLineAsync($"METHOD:{EncodeValue(calendar.Method)}");
+
+        if (!string.IsNullOrEmpty(calendar.Name))
+        {
+            await textWriter.WriteLineAsync($"NAME:{EncodeValue(calendar.Name)}");
+            await textWriter.WriteLineAsync($"X-WR-CALNAME:{EncodeValue(calendar.Name)}");
+        }
+
+        if (!string.IsNullOrEmpty(calendar.Description))
+        {
+            await textWriter.WriteLineAsync($"DESCRIPTION:{EncodeValue(calendar.Description)}");
+            await textWriter.WriteLineAsync($"X-WR-CALDESC:{EncodeValue(calendar.Description)}");
+        }
+
+        await textWriter.WriteLineAsync($"TIMEZONE-ID:{TimeZoneInfo.Local.StandardName}"); // Europe/London
+        await textWriter.WriteLineAsync($"X-WR-TIMEZONE:{TimeZoneInfo.Local.StandardName}"); // Europe/London
+
+        if (calendar.RefreshInterval != null)
+        {
+            await textWriter.WriteLineAsync($"REFRESH-INTERVAL;VALUE=DURATION:{FormatTimeSpanAsInterval(calendar.RefreshInterval.Value)}");
+            await textWriter.WriteLineAsync($"X-PUBLISHED-TTL:{FormatTimeSpanAsInterval(calendar.RefreshInterval.Value)}");
+        }
+
+        foreach (var calendarEvent in calendar.Events)
+        {
+            token.ThrowIfCancellationRequested();
+            await WriteToStream(calendarEvent, textWriter);
+        }
+
+        await textWriter.WriteLineAsync("END:VCALENDAR");
+    }
+
+    private static async Task WriteToStream(CalendarEvent calendarEvent, TextWriter textWriter)
+    {
+        await textWriter.WriteLineAsync("BEGIN:VEVENT");
+        await textWriter.WriteLineAsync($"SUMMARY:{EncodeValue(calendarEvent.Title)}");
+        await textWriter.WriteLineAsync($"UID:{calendarEvent.Id}");
+        await textWriter.WriteLineAsync($"SEQUENCE:{calendarEvent.Version}");
+        await textWriter.WriteLineAsync($"STATUS:{(calendarEvent.Confirmed ? "CONFIRMED" : "TENTATIVE")}");
+        await textWriter.WriteLineAsync($"TRANSP:{(calendarEvent.Private ? "OPAQUE" : "TRANSPARENT")}");
+        await textWriter.WriteLineAsync($"DTSTART:{FormatDateTime(calendarEvent.FromInclusive)}");
+        await textWriter.WriteLineAsync($"DTEND:{FormatDateTime(calendarEvent.ToExclusive)}");
+        await textWriter.WriteLineAsync($"DTSTAMP:{FormatDateTime(calendarEvent.LastUpdated)}");
+        if (calendarEvent.Categories.Count > 0)
+        {
+            await textWriter.WriteLineAsync($"CATEGORIES:{string.Join(",", calendarEvent.Categories.Select(EncodeValue))}");
+        }
+
+        if (calendarEvent.Url != null)
+        {
+            await textWriter.WriteLineAsync($"URL:{EncodeValue(calendarEvent.Url.ToString())}");
+        }
+
+        if (!string.IsNullOrEmpty(calendarEvent.Location))
+        {
+            await textWriter.WriteLineAsync($"LOCATION:{EncodeValue(calendarEvent.Location)}");
+        }
+
+        await textWriter.WriteLineAsync("END:VEVENT");
+    }
+
+    private static string FormatDateTime(DateTime dateTime)
+    {
+        return dateTime.ToString("yyyyMMddTHHmmss");
+    }
+
+    private static string FormatTimeSpanAsInterval(TimeSpan interval)
+    {
+        return $"P{interval.TotalHours}H";
+    }
+
+    private static string EncodeValue(string value)
+    {
+        // See https://icalendar.org/iCalendar-RFC-5545/3-1-content-lines.html
+
+        var singleLine = value.Trim().Replace("\r", "").Replace("\n", "\\n");
+        // split the line into 70char blocks, with a ' ' at the start of any following line
+        var lines = new List<string>();
+        while (singleLine.Length > 70)
+        {
+            var line = singleLine.Substring(0, 70);
+            if (line.EndsWith("\\") && singleLine.Length >= 71)
+            {
+                // include the n from a \\n in the block
+                line += singleLine.Substring(70, 1);
+            }
+
+            lines.Add(line);
+
+            singleLine = singleLine.Substring(line.Length);
+        }
+
+        if (singleLine.Length > 0)
+        {
+            lines.Add(singleLine);
+        }
+
+        return string.Join("\n", lines.Select((line, index) => index == 0 ? line : " " + line));
+    }
+}
