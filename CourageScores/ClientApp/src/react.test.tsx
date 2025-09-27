f327cb9af320b3aa8900c81bdf27968c7fc97597
Simon Laing <laingster@gmail.com>
2025-09-27 10:29:33 +0100

    poc: Make external/real http calls

---
+++ CourageScores/ClientApp/src/react.test.tsx
@@ -0,0 +1,11 @@
+﻿import fetch from 'isomorphic-fetch';
+
+describe('testing', () => {
+    it('can make a http request', async () => {
+        const response = await fetch(
+            'https://courageleague.azurewebsites.net/data/api/Status',
+        );
+        const json = await response.json();
+        expect(json.success).toEqual(true);
+    });
+});
