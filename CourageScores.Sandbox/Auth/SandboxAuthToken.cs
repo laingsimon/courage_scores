d9db70e708a8f9aa2c61aca966122b8d7334857b
Simon Laing <laingster@gmail.com>
2025-10-14 19:47:35 +0100

    Add ability to run a sandboxed version of the app

---
+++ CourageScores.Sandbox/Auth/SandboxAuthToken.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Claims;
+using Newtonsoft.Json;
+
+namespace CourageScores.Sandbox.Auth;
+
+internal record SandboxAuthToken(string Name, string GivenName, string EmailAddress)
+{
+    public static SandboxAuthToken FromPrincipal(ClaimsPrincipal principal)
+    {
+        var claims = principal.Identities
+            .SelectMany(i => i.Claims)
+            .ToDictionary(c => c.Type, c => c.Value);
+
+        return new SandboxAuthToken(
+            claims[ClaimTypes.Name],
+            claims[ClaimTypes.GivenName],
+            claims[ClaimTypes.Email]);
+    }
+
+    public static SandboxAuthToken? Deserialise(string json)
+    {
+        try
+        {
+            return JsonConvert.DeserializeObject<SandboxAuthToken>(json)!;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public string Serialise()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+
+    public ClaimsPrincipal ToPrincipal()
+    {
+        return new ClaimsPrincipal(new ClaimsIdentity(
+        [
+            new Claim(ClaimTypes.Name, Name),
+            new Claim(ClaimTypes.GivenName, GivenName),
+            new Claim(ClaimTypes.Email, EmailAddress),
+        ]));
+    }
+}
---
+++ CourageScores.Sandbox/Cosmos/SandboxCosmosDatabaseFactory.cs
@@ -0,0 +1,39 @@
+﻿using CourageScores.Repository;
+using CourageScores.Repository.Identity;
+using CourageScores.Sandbox.Auth;
+using CourageScores.StubCosmos.Api;
+using Microsoft.Azure.Cosmos;
+
+namespace CourageScores.Sandbox.Cosmos;
+
+public class SandboxCosmosDatabaseFactory : ICosmosDatabaseFactory, IStubCosmosData
+{
+    private Database? _database;
+    private readonly ICosmosDatabaseFactory _stubDatabaseFactory;
+
+    public SandboxCosmosDatabaseFactory(StubCosmosDatabaseFactory stubDatabaseFactory)
+    {
+        _stubDatabaseFactory = stubDatabaseFactory;
+    }
+
+    public async Task<Database> CreateDatabase()
+    {
+        return _database ??= await CreateDatabaseWithAdminUser();
+    }
+
+    private async Task<Database> CreateDatabaseWithAdminUser()
+    {
+        var database = await _stubDatabaseFactory.CreateDatabase();
+        var userRepo = new UserRepository(database);
+
+        await TestAuthenticationService.AddAdminUserToContainer(userRepo);
+
+        return database;
+    }
+
+    public Task Clear()
+    {
+        _database = null;
+        return Task.CompletedTask;
+    }
+}
