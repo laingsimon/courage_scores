cfb60da1aa7caa51a9dbaf4b3a421a06640f5fca
Simon Laing <laingsimon@users.noreply.github.com>
2025-11-03 17:32:55 +0000

    Update playwright.config.ts

--- CourageScores.BehaviouralTests/playwright.config.ts
+++ CourageScores.BehaviouralTests/playwright.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
         ignoreHTTPSErrors: true,
         reuseExistingServer: true,
         timeout: 30_000,
-        stdout: 'pipe',
-        // stderr: 'pipe',
+        // stdout: 'pipe',
+        stderr: 'pipe',
     },
 });
