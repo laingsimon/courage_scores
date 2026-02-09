6a038c5587e12970daebb3972f8be55909fdca37
Simon Laing <laingster@gmail.com>
2026-02-09 13:11:02 +0000

    #1965: Capture errors during render

--- CourageScores/ClientApp/src/components/admin/Query.tsx
+++ CourageScores/ClientApp/src/components/admin/Query.tsx
@@ -117,92 +117,109 @@ export function Query() {
     }
 
     const headingLookup = getHeadingLookup(results?.result?.rows);
-    return (
-        <div className="content-background p-3">
-            <div className="input-group mb-3">
-                <div className="d-flex pe-3 align-self-center fs-5">Query</div>
-                <BootstrapDropdown
-                    onChange={(container) =>
-                        changeParams('container', container!)
-                    }
-                    options={containerOptions}
-                    value={container || ''}
-                />
-                <div className="input-group-prepend ms-1">
-                    <span className="input-group-text">Limit</span>
+
+    try {
+        return (
+            <div className="content-background p-3">
+                <div className="input-group mb-3">
+                    <div className="d-flex pe-3 align-self-center fs-5">
+                        Query
+                    </div>
+                    <BootstrapDropdown
+                        onChange={(container) =>
+                            changeParams('container', container!)
+                        }
+                        options={containerOptions}
+                        value={container || ''}
+                    />
+                    <div className="input-group-prepend ms-1">
+                        <span className="input-group-text">Limit</span>
+                    </div>
+                    <input
+                        type="number"
+                        className="form-control"
+                        placeholder="Max"
+                        value={max || ''}
+                        min={1}
+                        onChange={(e) => changeParams('max', e.target.value)}
+                    />
+                    <button
+                        className="btn btn-primary ms-1"
+                        onClick={onExecute}>
+                        {executing ? <LoadingSpinnerSmall /> : null}
+                        Execute
+                    </button>
                 </div>
-                <input
-                    type="number"
-                    className="form-control"
-                    placeholder="Max"
-                    value={max || ''}
-                    min={1}
-                    onChange={(e) => changeParams('max', e.target.value)}
-                />
-                <button className="btn btn-primary ms-1" onClick={onExecute}>
-                    {executing ? <LoadingSpinnerSmall /> : null}
-                    Execute
-                </button>
-            </div>
-            <div>
-                <textarea
-                    className="width-100 font-monospace form-control"
-                    name="query"
-                    value={volatileQuery}
-                    rows={8}
-                    onChange={stateChanged(setVollatileQuery)}
-                    onBlur={() => changeParams('query', volatileQuery)}
-                />
+                <div>
+                    <textarea
+                        className="width-100 font-monospace form-control"
+                        name="query"
+                        value={volatileQuery}
+                        rows={8}
+                        onChange={stateChanged(setVollatileQuery)}
+                        onBlur={() => changeParams('query', volatileQuery)}
+                    />
 
-                {results ? null : <pre>Examples: select game.id from game</pre>}
-            </div>
-            <div>
-                {results?.success === true && results.result ? (
-                    <div>
-                        <h5>
-                            {results.result.rows.length} of{' '}
-                            {results.result.rowCount} rows
-                        </h5>
+                    {results ? null : (
+                        <pre>Examples: select game.id from game</pre>
+                    )}
+                </div>
+                <div>
+                    {results?.success === true && results.result ? (
+                        <div>
+                            <h5>
+                                {results.result.rows.length} of{' '}
+                                {results.result.rowCount} rows
+                            </h5>
 
-                        <table className="table table-sm table-striped">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    {headingLookup.map((heading, index) => (
-                                        <th key={index}>{heading}</th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {results.result.rows.map((row, rowIndex) => (
-                                    <tr key={rowIndex}>
-                                        <td>{rowIndex + 1}</td>
+                            <table className="table table-sm table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
                                         {headingLookup.map((heading, index) => (
-                                            <td key={index}>
-                                                {renderValue(row[heading])}
-                                            </td>
+                                            <th key={index}>{heading}</th>
                                         ))}
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                ) : null}
-                {any(results?.errors) ? (
-                    <div className="alert alert-danger">
-                        {results!.errors?.map((e, i) => (
-                            <li key={i}>{e}</li>
-                        ))}
-                    </div>
-                ) : null}
-                {any(results?.warnings) ? (
-                    <div className="alert alert-warning">
-                        {results!.warnings?.map((w, i) => (
-                            <li key={i}>{w}</li>
-                        ))}
-                    </div>
-                ) : null}
+                                </thead>
+                                <tbody>
+                                    {results.result.rows.map(
+                                        (row, rowIndex) => (
+                                            <tr key={rowIndex}>
+                                                <td>{rowIndex + 1}</td>
+                                                {headingLookup.map(
+                                                    (heading, index) => (
+                                                        <td key={index}>
+                                                            {renderValue(
+                                                                row[heading],
+                                                            )}
+                                                        </td>
+                                                    ),
+                                                )}
+                                            </tr>
+                                        ),
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    ) : null}
+                    {any(results?.errors) ? (
+                        <div className="alert alert-danger">
+                            {results!.errors?.map((e, i) => (
+                                <li key={i}>{e}</li>
+                            ))}
+                        </div>
+                    ) : null}
+                    {any(results?.warnings) ? (
+                        <div className="alert alert-warning">
+                            {results!.warnings?.map((w, i) => (
+                                <li key={i}>{w}</li>
+                            ))}
+                        </div>
+                    ) : null}
+                </div>
             </div>
-        </div>
-    );
+        );
+    } catch (e) {
+        onError(e);
+    }
 }
