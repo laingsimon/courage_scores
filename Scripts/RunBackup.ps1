e444d0737b9c1c70a6c61f629982a3e8c6122841
Simon Laing <laingster@gmail.com>
2026-04-25 14:19:35 +0100

    #2113: Refactor bootstrap script

--- Scripts/RunBackup.ps1
+++ Scripts/RunBackup.ps1
@@ -1,11 +1,14 @@
+$Source = "https://courageleague.azurewebsites.net/data/api/Data/Backup/"
+$Destination = "https://courageleagueuat.azurewebsites.net/data/api/Data/Restore/"
+$Identity = "prod_backup"
+
+$PathToScript = "./Downloaded-BackupToUat.ps1"
+
 function Get-BackupScript()
 {
     $ScriptUrl = "https://raw.githubusercontent.com/laingsimon/courage_scores/refs/heads/main/Scripts/BackupToUat.ps1"
-    $PathToScript = "./Downloaded-BackupToUat.ps1"
 
     Invoke-WebRequest -UseDefaultCredentials -Uri $ScriptUrl -Method GET -OutFile $PathToScript
-
-    return $PathToScript
 }
 
 $PathToScript = Get-BackupScript
@@ -14,9 +17,6 @@ Write-Output "Script written to $($PathToScript)"
 try
 {
     Write-Output "Running the backup..."
-    $Source = "https://courageleague.azurewebsites.net/data/api/Data/Backup/"
-    $Destination = "https://courageleagueuat.azurewebsites.net/data/api/Data/Restore/"
-    $Identity = "prod_backup"
 
     ./Downloaded-BackupToUat.ps1 -source $Source -destination $Destination -identity $Identity
 }
