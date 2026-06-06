0f3f04724d8cbd8adc330af4038fb0b409b11994
Simon Laing <laingster@gmail.com>
2026-06-06 09:21:34 +0100

    #2237: Change properties to init only

--- CourageScores/Models/Dtos/IIntegrityCheckDto.cs
+++ CourageScores/Models/Dtos/IIntegrityCheckDto.cs
@@ -5,5 +5,5 @@ public interface IIntegrityCheckDto
     /// <summary>
     /// Token to ensure this update doesn't overwrite any previous updates since the last reload
     /// </summary>
-    DateTime? LastUpdated { get; set; }
+    DateTime? LastUpdated { get; }
 }
