8e995bf061b947c997ba20ed2ce0909c7053d22b
Simon Laing <laingster@gmail.com>
2026-06-13 14:17:50 +0100

    #2281: Default to showing future fixtures from menu

--- CourageScores/ClientApp/src/components/layout/NavMenu.test.tsx
+++ CourageScores/ClientApp/src/components/layout/NavMenu.test.tsx
@@ -159,7 +159,7 @@ describe('NavMenu', () => {
             ).toEqual([
                 'https://localhost/BEFORE1',
                 'https://localhost/BEFORE2',
-                `http://localhost/fixtures/${currentSeason.name}/?division=${division.name}`,
+                `http://localhost/fixtures/${currentSeason.name}/?division=${division.name}&date=future`,
                 'https://localhost/AFTER1',
                 'https://localhost/AFTER2',
                 `http://localhost/login/?redirectUrl=${encodeURIComponent('/practice?q=value')}`,
