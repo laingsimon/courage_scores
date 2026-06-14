39102ff119e1cab0f262b0e9514230df1f190030
Simon Laing <laingster@gmail.com>
2026-06-14 19:28:42 +0100

    #2284: Require container name when interacting with blob storage

--- CourageScores/Repository/BlobStorageRepository.cs
+++ CourageScores/Repository/BlobStorageRepository.cs
@@ -7,23 +7,21 @@ namespace CourageScores.Repository;
 [ExcludeFromCodeCoverage]
 public class BlobStorageRepository : IBlobStorageRepository
 {
-    private readonly BlobContainerClient _photosClient;
+    private readonly BlobServiceClient _blobServiceClient;
 
     public BlobStorageRepository(IConfiguration configuration)
     {
         var accountName = configuration["BlobStorage_AccountName"];
         var key = configuration["BlobStorage_Key"];
 
-        var blobServiceClient = new BlobServiceClient(
+        _blobServiceClient = new BlobServiceClient(
             new Uri($"https://{accountName}.blob.core.windows.net"),
             new StorageSharedKeyCredential(accountName, key));
-        _photosClient = blobServiceClient.GetBlobContainerClient("photos");
-        _photosClient.CreateIfNotExists();
     }
 
-    public async Task<byte[]?> Read(string path, CancellationToken token)
+    public async Task<byte[]?> Read(string container, string path, CancellationToken token)
     {
-        var blob = _photosClient.GetBlobClient(path);
+        var blob = GetContainer(container).GetBlobClient(path);
         if (!await blob.ExistsAsync(cancellationToken: token))
         {
             return null;
@@ -39,16 +37,23 @@ public class BlobStorageRepository : IBlobStorageRepository
         return memoryStream.ToArray();
     }
 
-    public async Task Write(string path, byte[] contents, CancellationToken token)
+    public async Task Write(string container, string path, byte[] contents, CancellationToken token)
     {
-        var blob = _photosClient.GetBlobClient(path);
+        var blob = GetContainer(container).GetBlobClient(path);
         var stream = new MemoryStream(contents);
         await blob.UploadAsync(stream, true, token);
     }
 
-    public async Task Delete(string path, CancellationToken token)
+    public async Task Delete(string container, string path, CancellationToken token)
     {
-        var blob = _photosClient.GetBlobClient(path);
+        var blob = GetContainer(container).GetBlobClient(path);
         await blob.DeleteIfExistsAsync(cancellationToken: token);
+    }
+
+    private BlobContainerClient GetContainer(string container)
+    {
+        var client = _blobServiceClient.GetBlobContainerClient(container);
+        client.CreateIfNotExists();
+        return client;
     }
 }
