8db8920efdd689990d79ab509fd1f12e95e39f67
Simon Laing <laingster@gmail.com>
2026-09-05 08:20:55 +0100

    #2434: Don't clear custom permissions if custom is releselected

--- CourageScores/ClientApp/src/components/admin/UserAccessOption.tsx
+++ CourageScores/ClientApp/src/components/admin/UserAccessOption.tsx
@@ -224,9 +224,11 @@ export function UserAccessOption({
             <BootstrapDropdown
                 options={options}
                 value={value}
-                onChange={async (value: OptionValue) =>
-                    await accessChanged(option, valueLookup[value])
-                }
+                onChange={async (newValue: OptionValue) => {
+                    if (newValue !== value) {
+                        await accessChanged(option, valueLookup[newValue]);
+                    }
+                }}
                 slim={true}
                 className="margin-right"
                 datatype={option}
