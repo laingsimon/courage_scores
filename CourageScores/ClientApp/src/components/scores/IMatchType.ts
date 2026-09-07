5456b3ec95c5344ba4b2b6d00d212ea227a5dab7
Simon Laing <laingster@gmail.com>
2026-09-07 08:31:02 +0100

    #2434: Reduce complexity of Score component

--- CourageScores/ClientApp/src/components/scores/IMatchType.ts
+++ CourageScores/ClientApp/src/components/scores/IMatchType.ts
@@ -1,7 +1,7 @@
 import { TeamPlayerDto } from '../../interfaces/models/dtos/Team/TeamPlayerDto.ts';
 import { GameMatchDto } from '../../interfaces/models/dtos/Game/GameMatchDto.ts';
 import { GameMatchOptionDto } from '../../interfaces/models/dtos/Game/GameMatchOptionDto.ts';
-import { ICreatePlayerFor } from './Score.ts';
+import { ICreatePlayerFor } from './ScoreCardCreatePlayerDialog.tsx';
 import { UntypedPromise } from '../../interfaces/UntypedPromise.ts';
 
 export interface IMatchType {
