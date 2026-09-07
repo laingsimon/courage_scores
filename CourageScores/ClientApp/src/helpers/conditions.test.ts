8fd187520bafd7bbf94e0d747ea28b4f7cfc43c4
Simon Laing <laingster@gmail.com>
2026-09-07 07:44:51 +0100

    #2434: Add helper method for frontend to interrogate granular permissions

--- CourageScores/ClientApp/src/helpers/conditions.test.ts
+++ CourageScores/ClientApp/src/helpers/conditions.test.ts
@@ -1,6 +1,15 @@
-﻿import { hasAccess, hasAllAccess, hasAnyAccess } from './conditions.ts';
+﻿import {
+    hasAccess,
+    hasAccessLevel,
+    hasAllAccess,
+    hasAllAccessLevels,
+    hasAnyAccess,
+    hasAnyAccessLevel,
+    UserAccessContext,
+} from './conditions.ts';
 import { AccessOption } from '../interfaces/models/dtos/Identity/AccessOption.ts';
 import { user } from './tests.tsx';
+import { AccessLevelDto } from '../interfaces/models/dtos/Identity/AccessLevelDto';
 
 describe('conditions', () => {
     const userTemplate = {
@@ -8,6 +17,270 @@ describe('conditions', () => {
         name: 'name',
         emailAddress: 'simon@email.com',
     };
+
+    describe('hasAccessLevel', () => {
+        const seasonId: string = '00000000-0000-0000-0000-111111111111';
+        const divisionId: string = '00000000-0000-0000-0000-222222222222';
+        const teamId: string = '00000000-0000-0000-0000-333333333333';
+        const otherId: string = '00000000-0000-0000-0000-444444444444';
+        const granularTestCaseOption: AccessOption =
+            AccessOption.analyseMatches;
+
+        interface GranularUserAccessTestCase {
+            level: AccessLevelDto;
+            context: UserAccessContext;
+            expected: boolean;
+        }
+
+        const buildGranularUserAccessTestCase = (
+            level: AccessLevelDto,
+            context: Partial<UserAccessContext>,
+            expected?: boolean,
+        ): GranularUserAccessTestCase & { toString: () => string } => ({
+            level,
+            context: { ...context, ...{ option: granularTestCaseOption } },
+            expected: expected ?? false,
+            toString: () =>
+                `${JSON.stringify(level).replaceAll('00000000-0000-0000-0000-', '...-')} + ${JSON.stringify(context).replaceAll('00000000-0000-0000-0000-', '...-')} = ${expected ?? false}`,
+        });
+
+        // These are a mirror of the test cases in AccessServiceTests.cs
+        const granularUserAccessTestCases: GranularUserAccessTestCase[] = [
+            buildGranularUserAccessTestCase(
+                { seasonIds: [seasonId] },
+                {},
+                true,
+            ),
+            buildGranularUserAccessTestCase(
+                { divisionIds: [divisionId] },
+                {},
+                true,
+            ),
+            buildGranularUserAccessTestCase({ teamIds: [teamId] }, {}, true),
+            buildGranularUserAccessTestCase({}, { seasonId }, true),
+            buildGranularUserAccessTestCase({}, { seasonId, divisionId }, true),
+            buildGranularUserAccessTestCase(
+                {},
+                { seasonId, divisionId, teamId },
+                true,
+            ),
+            buildGranularUserAccessTestCase(
+                { seasonIds: [seasonId] },
+                { seasonId },
+                true,
+            ),
+            buildGranularUserAccessTestCase(
+                { divisionIds: [divisionId] },
+                { seasonId, divisionId },
+                true,
+            ),
+            buildGranularUserAccessTestCase(
+                { teamIds: [teamId] },
+                { seasonId, divisionId, teamId },
+                true,
+            ),
+            buildGranularUserAccessTestCase(
+                { seasonIds: [seasonId] },
+                { seasonId: otherId },
+            ),
+            buildGranularUserAccessTestCase(
+                { divisionIds: [divisionId] },
+                { seasonId, divisionId: otherId },
+            ),
+            buildGranularUserAccessTestCase(
+                { teamIds: [teamId] },
+                { seasonId, divisionId, teamId: otherId },
+            ),
+            buildGranularUserAccessTestCase({ seasonIds: [] }, {}),
+            buildGranularUserAccessTestCase({ divisionIds: [] }, {}),
+            buildGranularUserAccessTestCase({ teamIds: [] }, {}),
+        ];
+
+        it('returns false when not logged in', () => {
+            const result = hasAccessLevel(undefined, {
+                option: AccessOption.manageAccess,
+            });
+
+            expect(result).toBe(false);
+        });
+
+        it('returns false when access levels is null', () => {
+            const result = hasAccessLevel(userTemplate, {
+                option: AccessOption.manageAccess,
+            });
+
+            expect(result).toBe(false);
+        });
+
+        it('returns false when access levels does not contain access', () => {
+            const result = hasAccessLevel(
+                {
+                    ...userTemplate,
+                    accessLevels: {},
+                },
+                { option: AccessOption.manageAccess },
+            );
+
+            expect(result).toBe(false);
+        });
+
+        it('returns false when access levels does not contain access', () => {
+            const result = hasAccessLevel(
+                {
+                    ...userTemplate,
+                    accessLevels: {
+                        [AccessOption.manageAccess]: undefined!,
+                    },
+                },
+                { option: AccessOption.manageAccess },
+            );
+
+            expect(result).toBe(false);
+        });
+
+        it('returns true when access levels contains non-null access', () => {
+            const result = hasAccessLevel(
+                {
+                    ...userTemplate,
+                    accessLevels: {
+                        [AccessOption.manageAccess]: {},
+                    },
+                },
+                { option: AccessOption.manageAccess },
+            );
+
+            expect(result).toBe(true);
+        });
+
+        // This is a mirror of the test in AccessServiceTests.cs
+        it.each(granularUserAccessTestCases)(
+            'granularUserTestCase: %s',
+            async (testCase: GranularUserAccessTestCase) => {
+                const { level, context, expected } = testCase;
+                const result = hasAccessLevel(
+                    {
+                        ...userTemplate,
+                        accessLevels: {
+                            [granularTestCaseOption]: level,
+                        },
+                    },
+                    context,
+                );
+
+                expect(result).toBe(expected);
+            },
+        );
+    });
+
+    describe('hasAnyAccessLevel', () => {
+        it('returns false when no options provided', () => {
+            const result = hasAnyAccessLevel({
+                ...userTemplate,
+                accessLevels: {
+                    [AccessOption.manageAccess]: {},
+                },
+            });
+
+            expect(result).toBe(false);
+        });
+
+        it('returns false when no option is not defined', () => {
+            const result = hasAnyAccessLevel(
+                {
+                    ...userTemplate,
+                    accessLevels: {},
+                },
+                { option: AccessOption.manageAccess },
+            );
+
+            expect(result).toBe(false);
+        });
+
+        it('returns true when option is defined', () => {
+            const result = hasAnyAccessLevel(
+                {
+                    ...userTemplate,
+                    accessLevels: {
+                        [AccessOption.manageAccess]: {},
+                    },
+                },
+                { option: AccessOption.manageAccess },
+            );
+
+            expect(result).toBe(true);
+        });
+
+        it('returns true when either option is defined', () => {
+            const result = hasAnyAccessLevel(
+                {
+                    ...userTemplate,
+                    accessLevels: {
+                        [AccessOption.manageAccess]: {},
+                    },
+                },
+                { option: AccessOption.manageAccess },
+                { option: AccessOption.manageDivisions },
+            );
+
+            expect(result).toBe(true);
+        });
+    });
+
+    describe('hasAllAccessLevels', () => {
+        it('returns false when no options provided', () => {
+            const result = hasAllAccessLevels({
+                ...userTemplate,
+                accessLevels: {
+                    [AccessOption.manageAccess]: {},
+                },
+            });
+
+            expect(result).toBe(false);
+        });
+
+        it('returns false when no option is not defined', () => {
+            const result = hasAllAccessLevels(
+                {
+                    ...userTemplate,
+                    accessLevels: {},
+                },
+                { option: AccessOption.manageAccess },
+            );
+
+            expect(result).toBe(false);
+        });
+
+        it('returns false when one option is not defined', () => {
+            const result = hasAllAccessLevels(
+                {
+                    ...userTemplate,
+                    accessLevels: {
+                        [AccessOption.manageAccess]: {},
+                    },
+                },
+                { option: AccessOption.manageAccess },
+                { option: AccessOption.manageDivisions },
+            );
+
+            expect(result).toBe(false);
+        });
+
+        it('returns true when all options are defined', () => {
+            const result = hasAllAccessLevels(
+                {
+                    ...userTemplate,
+                    accessLevels: {
+                        [AccessOption.manageAccess]: {},
+                        [AccessOption.manageDivisions]: {},
+                    },
+                },
+                { option: AccessOption.manageAccess },
+                { option: AccessOption.manageDivisions },
+            );
+
+            expect(result).toBe(true);
+        });
+    });
 
     describe('hasAccess', () => {
         it('returns false when not logged in', () => {
--- CourageScores/ClientApp/src/helpers/conditions.ts
+++ CourageScores/ClientApp/src/helpers/conditions.ts
@@ -7,6 +7,66 @@ export interface IAccessLevels {
     [key: string]: AccessLevelDto;
 }
 
+export interface UserAccessContext {
+    option: AccessOption;
+    seasonId?: string;
+    divisionId?: string;
+    teamId?: string;
+}
+
+const permitted = (accessLevelIds?: string[], contextId?: string): boolean => {
+    if (accessLevelIds?.length === 0) {
+        return false;
+    }
+
+    if (!accessLevelIds || !contextId) {
+        return true;
+    }
+
+    return accessLevelIds.includes(contextId);
+};
+
+export function hasAccessLevel(
+    account: UserDto | undefined,
+    context: UserAccessContext,
+): boolean {
+    const accessLevel = account?.accessLevels?.[context.option];
+    if (!accessLevel) {
+        return false;
+    }
+
+    const seasonPermitted = permitted(accessLevel.seasonIds, context.seasonId);
+    const divisionPermitted = permitted(
+        accessLevel.divisionIds,
+        context.divisionId,
+    );
+    const teamPermitted = permitted(accessLevel.teamIds, context.teamId);
+
+    return seasonPermitted && divisionPermitted && teamPermitted;
+}
+
+export function hasAllAccessLevels(
+    account: UserDto | undefined,
+    ...contexts: UserAccessContext[]
+): boolean {
+    if (contexts.length === 0) {
+        return false;
+    }
+
+    return all(contexts, (context) => hasAccessLevel(account, context));
+}
+
+export function hasAnyAccessLevel(
+    account: UserDto | undefined,
+    ...contexts: UserAccessContext[]
+): boolean {
+    if (contexts.length === 0) {
+        return false;
+    }
+
+    return any(contexts, (context) => hasAccessLevel(account, context));
+}
+
 export function hasAccess(
     account: UserDto | undefined,
     option: AccessOption,
