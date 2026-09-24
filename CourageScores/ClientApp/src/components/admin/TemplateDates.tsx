a6eda9af4b32ba7a5b0d2143c2a1f81cd693f187
Simon Laing <laingster@gmail.com>
2026-09-24 07:32:04 +0100

    #2198: Mid-season marker should be computed based on fixtures only

--- CourageScores/ClientApp/src/components/admin/TemplateDates.tsx
+++ CourageScores/ClientApp/src/components/admin/TemplateDates.tsx
@@ -117,7 +117,10 @@ export function TemplateDates({
             </li>
             {dates.map((d: DateTemplateDto, index: number) => (
                 <li className="list-group-item position-relative" key={index}>
-                    {index === Math.floor(dates.length / 2) ? (
+                    {index ===
+                    Math.floor(
+                        dates.filter((d) => any(d.fixtures)).length / 2,
+                    ) ? (
                         <div className="position-absolute left-negative-100 top-negative-15 bg-warning-subtle px-2">
                             Mid season &rarr;
                         </div>
